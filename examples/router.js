27866f7004fcd546282f15c6399b28944f17eeac
xuliangzhan <xu_liangzhan@163.com>
2019-05-01 17:31:13 +0800

    update

--- examples/router.js
+++ examples/router.js
@@ -37,7 +37,7 @@ import TableScrollSize from './views/table/scroll/ScrollSize.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
