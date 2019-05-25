90b3367432f15184b91de1b676a32d7b3d007474
xuliangzhan <xu_liangzhan@163.com>
2019-05-25 19:45:15 +0800

    update

--- examples/views/table/grid/Page.vue
+++ examples/views/table/grid/Page.vue
@@ -36,7 +36,7 @@ export default {
         total: 0,
         currentPage: 1,
         pageSize: 10,
-        pageSizes: [10, 20, 50, 100, 200, 500, 1000]
+        pageSizes: [10, 20, 50, 100, 200, 500]
       },
       tableColumn: [
         {
