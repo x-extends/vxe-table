cd15c1f3f39ee02714ec46e79cbc7df803efd927
xuliangzhan <xu_liangzhan@163.com>
2019-05-26 15:55:27 +0800

    update

--- examples/App.vue
+++ examples/App.vue
@@ -9,11 +9,11 @@
       <div class="aside">
         <ul class="nav-menu">
           <li v-for="(item, index) in tableList" :key="index">
-            <a class="nav-link" @click="linkEvent(item)">{{ item.lable }}</a>
+            <a class="nav-link" @click="linkEvent(item)">{{ item.label }}</a>
             <ul v-if="item.children" v-show="item.expand" class="nav-child-menu">
               <li v-for="(child, cIndex) in item.children" :key="cIndex">
-                <a class="nav-link disabled" v-if="child.disabled" title="后续版本会考虑支持该功能！">{{ child.lable }}</a>
-                <router-link v-else class="nav-link" :to="child.locat">{{ child.lable }}</router-link>
+                <a class="nav-link disabled" v-if="child.disabled" title="后续版本会考虑支持该功能！">{{ child.label }}</a>
+                <router-link v-else class="nav-link" :to="child.locat">{{ child.label }}</router-link>
               </li>
             </ul>
           </li>
@@ -38,18 +38,18 @@ export default {
       selected: null,
       tableList: [
         {
-          lable: '开发指南',
+          label: '开发指南',
           value: 'start',
           expand: false,
           children: [
             {
-              lable: '安装',
+              label: '安装',
               locat: {
                 name: 'TableInstall'
               }
             },
             {
-              lable: '快速上手',
+              label: '快速上手',
               locat: {
                 name: 'TableUse'
               }
@@ -57,150 +57,150 @@ export default {
           ]
         },
         {
-          lable: '基本功能',
+          label: '基本功能',
           value: 'base',
           expand: false,
           children: [
             {
-              lable: '基础',
+              label: '基础',
               demoUrl: 'https://jsrun.net/VrXKp',
               locat: {
                 name: 'TableBasic'
               }
             },
             {
-              lable: '尺寸',
+              label: '尺寸',
               demoUrl: 'https://jsrun.net/PmXKp',
               locat: {
                 name: 'TableSize'
               }
             },
             {
-              lable: '内容显示省略号',
+              label: '内容显示省略号',
               locat: {
                 name: 'TableOverflow'
               }
             },
             {
-              lable: '斑马线条纹',
+              label: '斑马线条纹',
               demoUrl: 'https://jsrun.net/zrXKp',
               locat: {
                 name: 'TableStripe'
               }
             },
             {
-              lable: '带边框',
+              label: '带边框',
               demoUrl: 'https://jsrun.net/QrXKp',
               locat: {
                 name: 'TableBorder'
               }
             },
             {
-              lable: '单元格样式',
+              label: '单元格样式',
               demoUrl: 'https://jsrun.net/EmXKp',
               locat: {
                 name: 'TableStyle'
               }
             },
             {
-              lable: '隐藏头部',
+              label: '隐藏头部',
               demoUrl: 'https://jsrun.net/7mXKp',
               locat: {
                 name: 'TableHeader'
               }
             },
             {
-              lable: '列宽拖动',
+              label: '列宽拖动',
               demoUrl: 'https://jsrun.net/5AXKp',
               locat: {
                 name: 'TableResizable'
               }
             },
             {
-              lable: '流体高度',
+              label: '流体高度',
               demoUrl: 'https://jsrun.net/smXKp',
               locat: {
                 name: 'TableMaxHeight'
               }
             },
             {
-              lable: '固定表头',
+              label: '固定表头',
               demoUrl: 'https://jsrun.net/JrXKp',
               locat: {
                 name: 'TableHeight'
               }
             },
             {
-              lable: '固定列',
+              label: '固定列',
               demoUrl: 'https://jsrun.net/TrXKp',
               locat: {
                 name: 'TableFixed'
               }
             },
             {
-              lable: '固定表头和列',
+              label: '固定表头和列',
               demoUrl: 'https://jsrun.net/8rXKp',
               locat: {
                 name: 'TableFixedFull'
               }
             },
             {
-              lable: '表头分组',
+              label: '表头分组',
               demoUrl: 'https://jsrun.net/7rXKp',
               locat: {
                 name: 'TableGroup'
               }
             },
             {
-              lable: '序号',
+              label: '序号',
               demoUrl: 'https://jsrun.net/xrXKp',
               locat: {
                 name: 'TableIndex'
               }
             },
             {
-              lable: '单选',
+              label: '单选',
               demoUrl: 'https://jsrun.net/9rXKp',
               locat: {
                 name: 'TableRadio'
               }
             },
             {
-              lable: '多选',
+              label: '多选',
               demoUrl: 'https://jsrun.net/erXKp',
               locat: {
                 name: 'TableSelection'
               }
             },
             {
-              lable: '排序',
+              label: '排序',
               demoUrl: 'https://jsrun.net/crXKp',
               locat: {
                 name: 'TableSort'
               }
             },
             {
-              lable: '筛选',
+              label: '筛选',
               demoUrl: 'https://jsrun.net/drXKp',
               locat: {
                 name: 'TableFilter'
               }
             },
             {
-              lable: '空数据',
+              label: '空数据',
               locat: {
                 name: 'TableEmpty'
               }
             },
             {
-              lable: '加载中',
+              label: '加载中',
               demoUrl: 'https://jsrun.net/GjXKp',
               locat: {
                 name: 'TableLoading'
               }
             },
             {
-              lable: '格式化内容',
+              label: '格式化内容',
               demoUrl: 'https://jsrun.net/FrXKp',
               locat: {
                 name: 'TableFormat'
@@ -209,54 +209,54 @@ export default {
           ]
         },
         {
-          lable: '高级功能',
+          label: '高级功能',
           value: 'advanced',
           expand: false,
           children: [
             {
-              lable: '自定义模板',
+              label: '自定义模板',
               demoUrl: 'https://jsrun.net/DjXKp',
               locat: {
                 name: 'TableTemplate'
               }
             },
             {
-              lable: '合并行或列',
+              label: '合并行或列',
               demoUrl: 'https://jsrun.net/5jXKp',
               locat: {
                 name: 'TableSpan'
               }
             },
             {
-              lable: '表尾合计',
+              label: '表尾合计',
               demoUrl: 'https://jsrun.net/dmXKp',
               locat: {
                 name: 'TableFooter'
               }
             },
             {
-              lable: '显示/隐藏列',
+              label: '显示/隐藏列',
               demoUrl: 'https://jsrun.net/PrXKp',
               locat: {
                 name: 'TableCustom'
               }
             },
             {
-              lable: '导出.csv',
+              label: '导出.csv',
               demoUrl: 'https://jsrun.net/cmXKp',
               locat: {
                 name: 'TableExport'
               }
             },
             {
-              lable: '快捷菜单',
+              label: '快捷菜单',
               demoUrl: 'https://jsrun.net/VjXKp',
               locat: {
                 name: 'TableMenu'
               }
             },
             {
-              lable: '展开行',
+              label: '展开行',
               demoUrl: 'https://jsrun.net/eRXKp',
               locat: {
                 name: 'TableExpand'
@@ -265,30 +265,30 @@ export default {
           ]
         },
         {
-          lable: 'Tree 表格',
+          label: 'Tree 表格',
           value: 'tree',
           expand: false,
           children: [
             {
-              lable: '基本树形',
+              label: '基本树形',
               locat: {
                 name: 'TableTreeBasic'
               }
             },
             {
-              lable: '单选树形',
+              label: '单选树形',
               locat: {
                 name: 'TableTreeRadio'
               }
             },
             {
-              lable: '多选树形',
+              label: '多选树形',
               locat: {
                 name: 'TableTreeSelection'
               }
             },
             {
-              lable: '复杂树形',
+              label: '复杂树形',
               disabled: true,
               locat: {
                 name: 'TableTreeBasic'
@@ -297,25 +297,25 @@ export default {
           ]
         },
         {
-          lable: 'Grid 表格',
+          label: 'Grid 表格',
           value: 'grid',
           expand: false,
           children: [
             {
-              lable: '基础',
+              label: '基础',
               demoUrl: 'https://jsrun.net/5RXKp',
               locat: {
                 name: 'GridBasic'
               }
             },
             {
-              lable: '带分页',
+              label: '带分页',
               locat: {
                 name: 'GridPage'
               }
             },
             {
-              lable: '完整功能',
+              label: '完整功能',
               locat: {
                 name: 'GridFull'
               }
@@ -323,43 +323,43 @@ export default {
           ]
         },
         {
-          lable: 'Big 表格',
+          label: 'Big 表格',
           value: 'scroll',
           expand: false,
           children: [
             {
-              lable: '海量数据',
+              label: '海量数据',
               demoUrl: 'https://jsrun.net/XRXKp',
               locat: {
                 name: 'TableScroll'
               }
             },
             {
-              lable: '1 万行',
+              label: '1 万行',
               locat: {
                 name: 'TableScrollRows'
               }
             },
             {
-              lable: '10 万行 + 更复杂渲染',
+              label: '10 万行 + 更复杂渲染',
               locat: {
                 name: 'TableScrollFullRows'
               }
             },
             {
-              lable: '1 万行 1 万列',
+              label: '1 万行 1 万列',
               locat: {
                 name: 'TableScrollCols'
               }
             },
             {
-              lable: '10 万行 1 万列 + 更复杂渲染',
+              label: '10 万行 1 万列 + 更复杂渲染',
               locat: {
                 name: 'TableScrollFullCols'
               }
             },
             {
-              lable: '无限滚动',
+              label: '无限滚动',
               disabled: true,
               locat: {
                 name: 'TableScroll'
@@ -368,84 +368,84 @@ export default {
           ]
         },
         {
-          lable: 'Edit 表格',
+          label: 'Edit 表格',
           value: 'edit',
           expand: false,
           children: [
             {
-              lable: '手动触发',
+              label: '手动触发',
               locat: {
                 name: 'TableEditManual'
               }
             },
             {
-              lable: '点击触发',
+              label: '点击触发',
               locat: {
                 name: 'TableEditClick'
               }
             },
             {
-              lable: '双击触发',
+              label: '双击触发',
               locat: {
                 name: 'TableEditDBLClick'
               }
             },
             {
-              lable: '插入数据',
+              label: '插入数据',
               locat: {
                 name: 'TableEditInsert'
               }
             },
             {
-              lable: '删除数据',
+              label: '删除数据',
               locat: {
                 name: 'TableEditRemove'
               }
             },
             {
-              lable: '还原数据',
+              label: '还原数据',
               locat: {
                 name: 'TableEditRevert'
               }
             },
             {
-              lable: '编辑状态',
+              label: '编辑状态',
               locat: {
                 name: 'TableEditStatus'
               }
             },
             {
-              lable: '禁用编辑 cell',
+              label: '禁用编辑 cell',
               locat: {
                 name: 'TableEditCellDisable'
               }
             },
             {
-              lable: '禁用编辑 row',
+              label: '禁用编辑 row',
               locat: {
                 name: 'TableEditRowDisable'
               }
             },
             {
-              lable: '数据校验 cell',
+              label: '数据校验 cell',
               locat: {
                 name: 'TableEditCellValid'
               }
             },
             {
-              lable: '数据校验 row',
+              label: '数据校验 row',
               locat: {
                 name: 'TableEditRowValid'
               }
             },
             {
-              lable: '全键盘操作',
+              label: '全键盘操作',
               locat: {
                 name: 'TableEditKeyboard'
               }
             },
             {
-              lable: '自定义渲染',
+              label: '自定义渲染',
               locat: {
                 name: 'TableEditTemplate'
               }
@@ -453,12 +453,12 @@ export default {
           ]
         },
         {
-          lable: 'Excel 表格',
+          label: 'Excel 表格',
           value: 'excel',
           expand: false,
           children: [
             {
-              lable: '单元格',
+              label: '单元格',
               locat: {
                 name: 'TableExcelCell'
               }
@@ -466,24 +466,24 @@ export default {
           ]
         },
         {
-          lable: 'UI 集成',
+          label: 'UI 集成',
           value: 'other',
           expand: false,
           children: [
             {
-              lable: 'element-ui 自定义渲染',
+              label: 'element-ui 自定义渲染',
               locat: {
                 name: 'TableOtherElement'
               }
             },
             {
-              lable: 'iview 自定义渲染',
+              label: 'iview 自定义渲染',
               locat: {
                 name: 'TableOtherIview'
               }
             },
             {
-              lable: '使用 sortablejs 拖拽行排序',
+              label: '使用 sortablejs 拖拽行排序',
               disabled: true,
               locat: {
                 name: 'TableInstall'
@@ -492,30 +492,30 @@ export default {
           ]
         },
         {
-          lable: 'Plugin 插件',
+          label: 'Plugin 插件',
           value: 'plugin',
           expand: false,
           children: [
             {
-              lable: 'element-ui 配置式插件',
+              label: 'element-ui 配置式插件',
               locat: {
                 name: 'TablePluginElementConfig'
               }
             },
             {
-              lable: 'element-ui 配置式插件 + 分页',
+              label: 'element-ui 配置式插件 + 分页',
               locat: {
                 name: 'TablePluginElementPage'
               }
             },
             {
-              lable: 'iview 配置式插件',
+              label: 'iview 配置式插件',
               locat: {
                 name: 'TablePluginIviewConfig'
               }
             },
             {
-              lable: 'iview 配置式插件 + 分页',
+              label: 'iview 配置式插件 + 分页',
               locat: {
                 name: 'TablePluginIviewPage'
               }
@@ -523,54 +523,54 @@ export default {
           ]
         },
         {
-          lable: 'API',
+          label: 'API',
           value: 'api',
           expand: false,
           children: [
             {
-              lable: 'vxe-table',
+              label: 'vxe-table',
               locat: {
                 name: 'TableAPI'
               }
             },
             {
-              lable: 'vxe-table-column',
+              label: 'vxe-table-column',
               locat: {
                 name: 'TableColumnAPI'
               }
             },
             {
-              lable: 'vxe-grid',
+              label: 'vxe-grid',
               locat: {
                 name: 'GridAPI'
               }
             },
             {
-              lable: 'vxe-excel',
+              label: 'vxe-excel',
               locat: {
                 name: 'ExcelAPI'
               }
             },
             {
-              lable: 'vxe-pagination',
+              label: 'vxe-pagination',
               locat: {
                 name: 'PaginationAPI'
               }
             },
             {
-              lable: 'vxe-radio',
+              label: 'vxe-radio',
               locat: {
                 name: 'RadioAPI'
               }
             },
             {
-              lable: 'vxe-checkbox',
+              label: 'vxe-checkbox',
               locat: {
                 name: 'CheckboxAPI'
               }
             },
             {
-              lable: 'vxe-input',
+              label: 'vxe-input',
               locat: {
                 name: 'InputAPI'
               }
