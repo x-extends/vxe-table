3b2958fa869928aa9c484e1d3fa04dfb085b39e9
xuliangzhan <xu_liangzhan@163.com>
2019-05-29 22:54:23 +0800

    update

--- examples/views/table/start/Advanced.vue
+++ examples/views/table/start/Advanced.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <h3>高级函数</h3>
-    <p>自定义渲染器</p>
+    <p>自定义渲染器 + 渲染函数</p>
     <pre>
       <code class="javascript">{{ demoCodes[0] }}</code>
       <code class="html">{{ demoCodes[1] }}</code>
     </pre>
-    <p>内置拦截器（对于自定义渲染的组件与内部事件存在冲突时可能会使用到）</p>
+    <p>自定义渲染器 + JSX</p>
     <pre>
       <code class="javascript">{{ demoCodes[2] }}</code>
+      <code class="html">{{ demoCodes[3] }}</code>
+    </pre>
+    <p>内置拦截器（对于自定义渲染的组件与内部事件存在冲突时可能会使用到）</p>
+    <pre>
+      <code class="javascript">{{ demoCodes[4] }}</code>
     </pre>
   </div>
 </template>
@@ -51,6 +56,32 @@ export default {
             return [
               h('span', row[column.property])
             ]
+          }
+        })
+        `,
+        `
+        <vxe-table
+          border
+          height="600"
+          :data.sync="tableData"
+          :edit-config="{key: 'id', trigger: 'click', mode: 'row'}">
+          <vxe-table-column type="selection" width="60" fixed="left"></vxe-table-column>
+          <vxe-table-column type="index" width="60" fixed="left"></vxe-table-column>
+          <vxe-table-column prop="name" label="Name" :edit-render="{name: 'MyCell'}"></vxe-table-column>
+        </vxe-table>
+        `,
+        `
+        import Vue from 'vue'
+        import VXETable from 'vxe-table'
+        import 'vxe-table/lib/index.css'
+
+        VXETable.renderer.add('MyCell', {
+          autofocus: '.my-cell',
+          renderEdit (h, editRender, { row, column }) {
+            return <input class="my-cell" text="text" value={row[column.property]} on-input={val => row[column.property] = val}/>
+          },
+          renderCell (h, editRender, { row, column }) {
+            return <span>{row[column.property]}</span>
           }
         })
         `,
