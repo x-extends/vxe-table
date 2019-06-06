74a649184cfe42f93120b252a34b92653dd081bd
xuliangzhan <xu_liangzhan@163.com>
2019-06-06 16:28:18 +0800

    update

--- examples/App.vue
+++ examples/App.vue
@@ -34,11 +34,11 @@
       <div class="aside">
         <ul class="nav-menu">
           <li v-for="(item, index) in tableList" :key="index">
-            <a class="nav-link" @click="linkEvent(item)">{{ $t(item.label) }}</a>
+            <a class="nav-link" @click="linkEvent(item)" :title="$t(item.label)">{{ $t(item.label) }}</a>
             <ul v-if="item.children" v-show="item.expand" class="nav-child-menu">
               <li v-for="(child, cIndex) in item.children" :key="cIndex">
                 <a class="nav-link disabled" v-if="child.disabled" title="后续版本会考虑支持该功能！">{{ $t(child.label) }}</a>
-                <router-link v-else class="nav-link" :to="child.locat">{{ $t(child.label) }}</router-link>
+                <router-link v-else class="nav-link" :to="child.locat" :title="$t(child.label)">{{ $t(child.label) }}</router-link>
               </li>
             </ul>
           </li>
