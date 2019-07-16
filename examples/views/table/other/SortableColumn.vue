9b1bf84e9b0c5865dd3e1d03e9df1e73d1d4196a
xuliangzhan <xu_liangzhan@163.com>
2019-07-16 10:31:38 +0800

    update

--- examples/views/table/other/SortableColumn.vue
+++ examples/views/table/other/SortableColumn.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <p>使用 <a class="link" href="https://www.npmjs.com/package/sortablejs" target="_blank">sortablejs</a> 实现列移动，由于操作了 Dom 节点所以需要指定 <table-column-api-link prop="column-key"/></p>
+    <p><table-api-link name="vxe-table"/> 方式：使用 <a class="link" href="https://www.npmjs.com/package/sortablejs" target="_blank">sortablejs</a> 实现列移动</p>
+    <p class="red">由于操作了 Dom 节点，请根据需要指定 <table-column-api-link prop="column-key"/></p>
 
-    <vxe-grid
+    <vxe-table
       border
+      ref="xTable1"
       class="sortable-column-demo"
-      :columns="tableColumn"
-      :data.sync="tableData"></vxe-grid>
+      :data.sync="tableData">
+      <vxe-table-column field="name" title="Name"></vxe-table-column>
+      <vxe-table-column field="sex" title="Sex"></vxe-table-column>
+      <vxe-table-column field="age" title="Age"></vxe-table-column>
+      <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column>
+    </vxe-table>
 
     <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
 
@@ -14,6 +20,23 @@
       <code class="xml">{{ demoCodes[0] }}</code>
       <code class="javascript">{{ demoCodes[1] }}</code>
       <code class="css">{{ demoCodes[2] }}</code>
+    </pre>
+
+    <p><grid-api-link name="vxe-grid"/> 方式，更加简单的配置式调用</p>
+
+    <vxe-grid
+      border
+      ref="xTable2"
+      class="sortable-column-demo"
+      :columns="tableColumn"
+      :data.sync="tableData"></vxe-grid>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[3] }}</code>
+      <code class="javascript">{{ demoCodes[4] }}</code>
+      <code class="css">{{ demoCodes[5] }}</code>
     </pre>
   </div>
 </template>
@@ -24,19 +47,74 @@ import Sortable from 'sortablejs'
 
 export default {
   data () {
+    window.aa = this
     return {
       tableColumn: [
-        { field: 'name', title: 'Name', columnKey: 'name' },
-        { field: 'role', title: 'Role', columnKey: 'role' },
-        { field: 'sex', title: 'Sex', columnKey: 'sex' },
-        { field: 'date3', title: 'Date', columnKey: 'date3', width: 120 },
-        { field: 'address', title: 'Address', columnKey: 'address', showOverflow: true }
+        { field: 'name', title: 'Name' },
+        { field: 'role', title: 'Role' },
+        { field: 'sex', title: 'Sex' },
+        { field: 'date3', title: 'Date', width: 120 },
+        { field: 'address', title: 'Address', showOverflow: true }
       ],
       tableData: [],
       demoCodes: [
         `
+        <vxe-table
+          border
+          ref="xTable"
+          class="sortable-column-demo"
+          :data.sync="tableData">
+          <vxe-table-column type="index" width="60"></vxe-table-column>
+          <vxe-table-column field="name" title="Name"></vxe-table-column>
+          <vxe-table-column field="sex" title="Sex"></vxe-table-column>
+          <vxe-table-column field="age" title="Age"></vxe-table-column>
+          <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column>
+        </vxe-table>
+        `,
+        `
+        export default {
+          data () {
+            return {
+              tableData: []
+            }
+          },
+          destroyed () {
+            if (this.sortable) {
+              this.sortable.destroy()
+            }
+          },
+          created () {
+            this.tableData = window.MOCK_DATA_LIST.slice(0, 6)
+          },
+          methods: {
+            columnDrop () {
+              this.$nextTick(() => {
+                let xTable = this.$refs.xTable
+                this.sortable = Sortable.create(xTable.$el.querySelector('.body--wrapper>.vxe-table--header .vxe-header--row'), {
+                  handle: '.vxe-header--column',
+                  onEnd: ({ newIndex, oldIndex }) => {
+                    let tableColumn = xTable.getColumns()
+                    let currRow = tableColumn.splice(oldIndex, 1)[0]
+                    tableColumn.splice(newIndex, 0, currRow)
+                    xTable.loadColumn(tableColumn)
+                  }
+                })
+              })
+            }
+          }
+        }
+        `,
+        `
+        .sortable-column-demo .vxe-header--row .vxe-header--column.sortable-ghost,
+        .sortable-column-demo .vxe-header--row .vxe-header--column.sortable-chosen {
+          background-color: #dfecfb;
+        }
+        `,
+        `
         <vxe-grid
           border
+          ref="xTable"
+          class="sortable-column-demo"
           :columns="tableColumn"
           :data.sync="tableData"></vxe-grid>
         `,
@@ -65,7 +143,8 @@ export default {
           methods: {
             columnDrop () {
               this.$nextTick(() => {
-                this.sortable = Sortable.create(this.$el.querySelector('.body--wrapper>.vxe-table--header .vxe-header--row'), {
+                let xTable = this.$refs.xTable
+                this.sortable = Sortable.create(xTable.$el.querySelector('.body--wrapper>.vxe-table--header .vxe-header--row'), {
                   handle: '.vxe-header--column',
                   onEnd: ({ newIndex, oldIndex }) => {
                     let currRow = this.tableColumn.splice(oldIndex, 1)[0]
@@ -88,7 +167,8 @@ export default {
   },
   created () {
     this.tableData = window.MOCK_DATA_LIST.slice(0, 6)
-    this.columnDrop()
+    this.columnDrop1()
+    this.columnDrop2()
   },
   mounted () {
     Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
@@ -96,9 +176,24 @@ export default {
     })
   },
   methods: {
-    columnDrop () {
+    columnDrop1 () {
       this.$nextTick(() => {
-        this.sortable = Sortable.create(this.$el.querySelector('.body--wrapper>.vxe-table--header .vxe-header--row'), {
+        let xTable = this.$refs.xTable1
+        this.sortable = Sortable.create(xTable.$el.querySelector('.body--wrapper>.vxe-table--header .vxe-header--row'), {
+          handle: '.vxe-header--column',
+          onEnd: ({ newIndex, oldIndex }) => {
+            let tableColumn = xTable.getColumns()
+            let currRow = tableColumn.splice(oldIndex, 1)[0]
+            tableColumn.splice(newIndex, 0, currRow)
+            xTable.loadColumn(tableColumn)
+          }
+        })
+      })
+    },
+    columnDrop2 () {
+      this.$nextTick(() => {
+        let xTable = this.$refs.xTable2
+        this.sortable = Sortable.create(xTable.$el.querySelector('.body--wrapper>.vxe-table--header .vxe-header--row'), {
           handle: '.vxe-header--column',
           onEnd: ({ newIndex, oldIndex }) => {
             let currRow = this.tableColumn.splice(oldIndex, 1)[0]
