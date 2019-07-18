5375bcfb2ee5d069056599a38581ae7b256a8073
xuliangzhan <xu_liangzhan@163.com>
2019-07-19 00:16:31 +0800

    update

--- examples/App.vue
+++ examples/App.vue
@@ -489,6 +489,12 @@ export default {
               }
             },
             {
+              label: 'app.aside.nav.expandRow',
+              locat: {
+                name: 'TableTreeExpand'
+              }
+            },
+            {
               label: 'app.aside.nav.crudToolbar',
               locat: {
                 name: 'TableTreeToolbar'
--- packages/cell/src/cell.js
+++ packages/cell/src/cell.js
@@ -26,9 +26,6 @@ export const Cell = {
         renMaps.renderCell = selectConfig && selectConfig.checkField ? (isTreeNode ? this.renderTreeSelectionCellByProp : this.renderSelectionCellByProp) : (isTreeNode ? this.renderTreeSelectionCell : this.renderSelectionCell)
         break
       case 'expand':
-        if (treeConfig) {
-          throw new Error('[vxe-table] Tree tables and expanded rows are not allowed to exist together.')
-        }
         renMaps.renderCell = this.renderExpandCell
         renMaps.renderData = this.renderExpandData
         break
