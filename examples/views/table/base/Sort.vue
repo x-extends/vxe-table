ff0224c6d8746c8b02bb0702c880ed30d7421a8b
xuliangzhan <xu_liangzhan@163.com>
2019-07-22 13:23:21 +0800

    update

--- examples/views/table/base/Sort.vue
+++ examples/views/table/base/Sort.vue
@@ -42,6 +42,29 @@
       <code class="xml">{{ demoCodes[2] }}</code>
       <code class="javascript">{{ demoCodes[3] }}</code>
     </pre>
+
+    <p>点击列头排序的实现，通过 <table-api-link prop="header-cell-click"/> 事件和 <table-api-link prop="sort"/> 方法就可以实现</p>
+
+    <vxe-table
+      border
+      highlight-hover-row
+      ref="xTable"
+      height="300"
+      :data.sync="tableData"
+      @header-cell-click="headerCellClickEvent">
+      <vxe-table-column type="index" width="60"></vxe-table-column>
+      <vxe-table-column field="name" title="Name"></vxe-table-column>
+      <vxe-table-column field="sex" title="Sex" sortable></vxe-table-column>
+      <vxe-table-column field="age" title="Age" sortable></vxe-table-column>
+      <vxe-table-column field="time" title="Time" sortable></vxe-table-column>
+    </vxe-table>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[4] }}</code>
+      <code class="javascript">{{ demoCodes[5] }}</code>
+    </pre>
   </div>
 </template>
 
@@ -102,6 +125,40 @@ export default {
             this.tableData = window.MOCK_DATA_LIST.slice(0, 50)
           }
         }
+        `,
+        `
+        <vxe-table
+          border
+          highlight-hover-row
+          ref="xTable"
+          height="300"
+          :data.sync="tableData"
+          @header-cell-click="headerCellClickEvent">
+          <vxe-table-column type="index" width="60"></vxe-table-column>
+          <vxe-table-column field="name" title="Name"></vxe-table-column>
+          <vxe-table-column field="sex" title="Sex" sortable></vxe-table-column>
+          <vxe-table-column field="age" title="Age" sortable></vxe-table-column>
+          <vxe-table-column field="time" title="Time" sortable></vxe-table-column>
+        </vxe-table>
+        `,
+        `
+        export default {
+          data () {
+            return {
+              tableData: []
+            }
+          },
+          created () {
+            this.tableData = window.MOCK_DATA_LIST.slice(0, 50)
+          },
+          methods: {
+            headerCellClickEvent ({ column, triggerResizable, triggerSort, triggerFilter }) {
+              if (!(triggerResizable || triggerSort || triggerFilter)) {
+                this.$refs.xTable.sort(column.property)
+              }
+            }
+          }
+        }
         `
       ]
     }
@@ -113,6 +170,14 @@ export default {
     Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
       hljs.highlightBlock(block)
     })
+  },
+  methods: {
+    headerCellClickEvent ({ column, triggerResizable, triggerSort, triggerFilter }) {
+      // 如果点击了列并且没触发对应的按钮、则手动排序、列宽拖动
+      if (!(triggerResizable || triggerSort || triggerFilter)) {
+        this.$refs.xTable.sort(column.property)
+      }
+    }
   }
 }
 </script>
