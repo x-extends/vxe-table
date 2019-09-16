82ac04cd74fea6adcbb72ae612f78874504ddf66
xuliangzhan <xu_liangzhan@163.com>
2019-09-16 21:20:05 +0800

    update

--- packages/tools/src/event.js
+++ packages/tools/src/event.js
@@ -1,4 +1,5 @@
 import XEUtils from 'xe-utils/methods/xe-utils'
+import DomTools from './dom'
 
 // 监听全局事件
 const wheelName = /Firefox/i.test(navigator.userAgent) ? 'DOMMouseScroll' : 'mousewheel'
@@ -21,11 +22,13 @@ export const GlobalEvent = {
   }
 }
 
-document.addEventListener('keydown', GlobalEvent.trigger, false)
-document.addEventListener('contextmenu', GlobalEvent.trigger, false)
-window.addEventListener('mousedown', GlobalEvent.trigger, false)
-window.addEventListener('blur', GlobalEvent.trigger, false)
-window.addEventListener('resize', GlobalEvent.trigger, false)
-window.addEventListener(wheelName, GlobalEvent.trigger, false)
+if (DomTools.browse.isDoc) {
+  document.addEventListener('keydown', GlobalEvent.trigger, false)
+  document.addEventListener('contextmenu', GlobalEvent.trigger, false)
+  window.addEventListener('mousedown', GlobalEvent.trigger, false)
+  window.addEventListener('blur', GlobalEvent.trigger, false)
+  window.addEventListener('resize', GlobalEvent.trigger, false)
+  window.addEventListener(wheelName, GlobalEvent.trigger, false)
+}
 
 export default GlobalEvent
