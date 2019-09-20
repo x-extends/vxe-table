ff7c6ab35e11c47a4a344f14fbcd2d540431efea
xuliangzhan <xu_liangzhan@163.com>
2019-09-20 19:26:07 +0800

    update

--- examples/api/grid.js
+++ examples/api/grid.js
@@ -288,7 +288,7 @@ XEUtils.eachTree(gridAPI, (item, index, obj, paths, parent) => {
     item.list = [
       {
         name: 'reload',
-        desc: '刷新数据，会重置分页；触发 ajax.query 方法',
+        desc: '刷新数据并恢复到初始状态；触发 ajax.query 方法',
         type: '',
         enum: '',
         defVal: '',
