3eccf08165fd1e6d49a88283bcf1733543a32afc
xuliangzhan <xu_liangzhan@163.com>
2019-10-04 23:04:14 +0800

    update

--- examples/views/table/scroll/ScrollFullCols.vue
+++ examples/views/table/scroll/ScrollFullCols.vue
@@ -4,7 +4,7 @@
       虚拟滚动渲染，加载 10 万行 1 万列，左右固定列<br>
       大数据不建议使用双向绑定的 <table-api-link name="data"/> 属性（vue 监听会大数据会短暂的卡顿），建议使用 <table-api-link prop="loadData"/>/<table-api-link prop="loadColumn"/> 函数<br>
       对于多选 type=<table-column-api-link prop="selection"/> 当数据量海量时应该绑定 <table-api-link prop="checkField"/> 属性渲染速度更快<br>
-      注意：如果要启用横向虚拟滚动，所有的列宽度必须一致，否则无法兼容
+      <span class="red">注意：如果要启用横向虚拟滚动，所有的列宽度必须一致，否则无法兼容</span>
     </p>
 
     <vxe-grid
