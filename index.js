ccc83dd140311e85ba6adc550e4df92347be6ac7
xuliangzhan <xu_liangzhan@163.com>
2019-10-10 22:40:15 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // 默认主题
 import './styles/index.scss'
 // 默认安装全部模块
-import './packages/vxe-table'
+import VXETable from './packages/vxe-table'
+
+export * from './packages/vxe-table'
+export default VXETable
