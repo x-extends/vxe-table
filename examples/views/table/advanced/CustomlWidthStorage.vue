9614755d5ea41e0622a3cb818778cc74ab028114
xuliangzhan <xu_liangzhan@163.com>
2019-11-13 20:18:16 +0800

    工具栏个性化列支持禁用

--- examples/views/table/advanced/CustomlWidthStorage.vue
+++ examples/views/table/advanced/CustomlWidthStorage.vue
@@ -2,10 +2,11 @@
   <div>
     <p class="tip">
       显示/隐藏列和列宽拖动保存功能，通过设置 <toolbar-api-link prop="id"/> 和 <toolbar-api-link prop="resizable"/>={storage: true} 参数开启列宽拖动 localStorage 保存功能<br>
+      还可以通过 <toolbar-api-link prop="checkMethod"/> 设置列禁止勾选<br>
       也可以通过配合 <table-api-link prop="resizable-change"/> 事件实现服务端保存
     </p>
 
-    <vxe-toolbar id="toolbar_demo5" :resizable="{storage: true}" :setting="{storage: true}">
+    <vxe-toolbar id="toolbar_demo5" :resizable="{storage: true}" :setting="{storage: true, checkMethod: checkColumnMethod}">
       <template v-slot:buttons>
         <vxe-button>按钮1</vxe-button>
         <vxe-button>按钮2</vxe-button>
@@ -42,7 +43,7 @@ export default {
       tableData: [],
       demoCodes: [
         `
-        <vxe-toolbar id="toolbar_demo5" :resizable="{storage: true}" :setting="{storage: true}">
+        <vxe-toolbar id="toolbar_demo5" :resizable="{storage: true}" :setting="{storage: true, checkMethod: checkColumnMethod}">
           <template v-slot:buttons>
             <vxe-button>按钮1</vxe-button>
             <vxe-button>按钮2</vxe-button>
@@ -70,6 +71,14 @@ export default {
           },
           created () {
             this.tableData = window.MOCK_DATA_LIST.slice(0, 20)
+          },
+          methods: {
+            checkColumnMethod ({ column }) {
+              if (column.property === 'role') {
+                return false
+              }
+              return true
+            }
           }
         }
         `
@@ -84,6 +93,14 @@ export default {
     Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
       hljs.highlightBlock(block)
     })
+  },
+  methods: {
+    checkColumnMethod ({ column }) {
+      if (column.property === 'role') {
+        return false
+      }
+      return true
+    }
   }
 }
 </script>
