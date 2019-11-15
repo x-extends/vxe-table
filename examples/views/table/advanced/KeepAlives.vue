31678b8517c3a5b3b5693d7d04e22ffc816a546e
xuliangzhan <xu_liangzhan@163.com>
2019-11-15 17:43:19 +0800

    更新文档

--- examples/views/table/advanced/KeepAlives.vue
+++ examples/views/table/advanced/KeepAlives.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="tip">keep-alive 切换，v-show 可以使用 <table-api-link prop="auto-resize"/> 或者 <table-api-link prop="sync-resize"/> 自动监听响应式宽，也可以手动调用 <table-api-link prop="recalculate"/> 重新计算宽度</p>
 
-    <p>
+    <p class="btns">
       <router-link class="link" :to="{name: 'TableKeepAliveTable1'}">表格1</router-link>
       <router-link class="link" :to="{name: 'TableKeepAliveTable2'}">表格2</router-link>
       <router-link class="link" :to="{name: 'TableKeepAliveTable3'}">表格3</router-link>
@@ -28,7 +28,7 @@ export default {
     return {
       demoCodes: [
         `
-        <p>
+        <p class="btns">
           <router-link class="link" :to="{name: 'TableKeepAliveTable1'}">表格1</router-link>
           <router-link class="link" :to="{name: 'TableKeepAliveTable2'}">表格2</router-link>
           <router-link class="link" :to="{name: 'TableKeepAliveTable3'}">表格3</router-link>
@@ -48,3 +48,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.btns .link {
+  margin-right: 20px;
+  color: #333;
+}
+.btns .link.router-link-exact-active {
+  color: #409eff;
+}
+</style>
