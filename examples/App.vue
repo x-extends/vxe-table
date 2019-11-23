16ec07c54f3aa768daf9e221056e174b99f27650
xuliangzhan <xu_liangzhan@163.com>
2019-11-23 15:29:34 +0800

    内部优化

--- examples/App.vue
+++ examples/App.vue
@@ -791,6 +791,12 @@ export default {
               }
             },
             {
+              label: 'app.aside.nav.fluidHeight',
+              locat: {
+                name: 'TableScrollMaxHeight'
+              }
+            },
+            {
               label: 'app.aside.nav.edit',
               demoUrl: 'https://jsrun.pro/MIWKp/edit',
               locat: {
---
+++ examples/views/table/scroll/MaxHeight.vue
@@ -0,0 +1,202 @@
+<template>
+  <div>
+    <p class="tip">最大高度，通过设置 <table-api-link prop="max-height"/> 启用，当数据少时自适应</p>
+
+    <vxe-table
+      border
+      resizable
+      show-overflow
+      ref="xTable1"
+      max-height="400"
+      :loading="loading">
+      <vxe-table-column type="checkbox" width="60"></vxe-table-column>
+      <vxe-table-column type="index" width="100"></vxe-table-column>
+      <vxe-table-column field="name" title="Name" sortable width="200"></vxe-table-column>
+      <vxe-table-column field="age" title="Age" width="200"></vxe-table-column>
+      <vxe-table-column field="sex" title="Sex" width="200"></vxe-table-column>
+      <vxe-table-column field="region" title="Region" width="200"></vxe-table-column>
+      <vxe-table-column field="address" title="Address" width="300" show-overflow></vxe-table-column>
+      <vxe-table-column field="updateTime" title="UpdateTime" width="200"></vxe-table-column>
+      <vxe-table-column field="createTime" title="CreateTime" width="200"></vxe-table-column>
+    </vxe-table>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[0] }}</code>
+      <code class="javascript">{{ demoCodes[1] }}</code>
+    </pre>
+
+    <p class="tip">当数据超过最大高度时自动显示滚动条</p>
+
+    <vxe-table
+      border
+      resizable
+      show-overflow
+      ref="xTable2"
+      max-height="400"
+      :loading="loading">
+      <vxe-table-column type="checkbox" width="60"></vxe-table-column>
+      <vxe-table-column type="index" width="100"></vxe-table-column>
+      <vxe-table-column field="name" title="Name" sortable width="200"></vxe-table-column>
+      <vxe-table-column field="age" title="Age" width="200"></vxe-table-column>
+      <vxe-table-column field="rate" title="Rate" width="200"></vxe-table-column>
+      <vxe-table-column field="time" title="Time" width="200"></vxe-table-column>
+      <vxe-table-column field="updateTime" title="UpdateTime" width="200"></vxe-table-column>
+      <vxe-table-column field="createTime" title="CreateTime" width="200"></vxe-table-column>
+    </vxe-table>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[2] }}</code>
+      <code class="javascript">{{ demoCodes[3] }}</code>
+    </pre>
+  </div>
+</template>
+
+<script>
+import hljs from 'highlight.js'
+
+export default {
+  data () {
+    return {
+      loading: false,
+      demoCodes: [
+        `
+        <vxe-table
+          border
+          resizable
+          show-overflow
+          ref="xTable"
+          max-height="400"
+          :loading="loading">
+          <vxe-table-column type="checkbox" width="60"></vxe-table-column>
+          <vxe-table-column type="index" width="100"></vxe-table-column>
+          <vxe-table-column field="name" title="Name" sortable width="200"></vxe-table-column>
+          <vxe-table-column field="age" title="Age" width="200"></vxe-table-column>
+          <vxe-table-column field="region" title="Region" width="200"></vxe-table-column>
+          <vxe-table-column field="address" title="Address" width="300" show-overflow></vxe-table-column>
+          <vxe-table-column field="updateTime" title="UpdateTime" width="200"></vxe-table-column>
+          <vxe-table-column field="createTime" title="CreateTime" width="200"></vxe-table-column>
+        </vxe-table>
+        `,
+        `
+        export default {
+          data () {
+            return {
+              loading: false
+            }
+          },
+          created () {
+            this.findList()
+          },
+          methods: {
+            findList () {
+              this.loading = true
+              return new Promise(resolve => {
+                setTimeout(() => {
+                  let tableData = window.MOCK_DATA_LIST.slice(0, 3)
+                  // 阻断 vue 对大数组的双向绑定，避免 vue 绑定大数据造成短暂的卡顿
+                  if (this.$refs.xTable) {
+                    this.$refs.xTable.loadData(tableData)
+                  }
+                  resolve()
+                  this.loading = false
+                }, 300)
+              })
+            }
+          }
+        }
+        `,
+        `
+        <vxe-table
+          border
+          resizable
+          show-overflow
+          ref="xTable"
+          max-height="400"
+          :loading="loading">
+          <vxe-table-column type="checkbox" width="60"></vxe-table-column>
+          <vxe-table-column type="index" width="100"></vxe-table-column>
+          <vxe-table-column field="name" title="Name" sortable width="200"></vxe-table-column>
+          <vxe-table-column field="age" title="Age" width="200"></vxe-table-column>
+          <vxe-table-column field="sex" title="Sex" width="200"></vxe-table-column>
+          <vxe-table-column field="rate" title="Rate" width="200"></vxe-table-column>
+          <vxe-table-column field="time" title="Time" width="200"></vxe-table-column>
+          <vxe-table-column field="updateTime" title="UpdateTime" width="200"></vxe-table-column>
+          <vxe-table-column field="createTime" title="CreateTime" width="200"></vxe-table-column>
+        </vxe-table>
+        `,
+        `
+        export default {
+          data () {
+            return {
+              loading: false
+            }
+          },
+          created () {
+            this.findList()
+          },
+          methods: {
+            findList () {
+              this.loading = true
+              return new Promise(resolve => {
+                setTimeout(() => {
+                  let tableData = window.MOCK_DATA_LIST.slice(0, 2000)
+                  // 阻断 vue 对大数组的双向绑定，避免 vue 绑定大数据造成短暂的卡顿
+                  if (this.$refs.xTable) {
+                    this.$refs.xTable.loadData(tableData)
+                  }
+                  resolve()
+                  this.loading = false
+                }, 300)
+              })
+            }
+          }
+        }
+        `
+      ]
+    }
+  },
+  created () {
+    this.findList1()
+    this.findList2()
+  },
+  mounted () {
+    Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
+      hljs.highlightBlock(block)
+    })
+  },
+  methods: {
+    findList1 () {
+      this.loading = true
+      return new Promise(resolve => {
+        setTimeout(() => {
+          let tableData = window.MOCK_DATA_LIST.slice(0, 3)
+          // 阻断 vue 对大数组的双向绑定，避免 vue 绑定大数据造成短暂的卡顿
+          if (this.$refs.xTable1) {
+            this.$refs.xTable1.loadData(tableData)
+          }
+          resolve()
+          this.loading = false
+        }, 300)
+      })
+    },
+    findList2 () {
+      this.loading = true
+      return new Promise(resolve => {
+        setTimeout(() => {
+          let tableData = window.MOCK_DATA_LIST.slice(0, 2000)
+          // 阻断 vue 对大数组的双向绑定，避免 vue 绑定大数据造成短暂的卡顿
+          if (this.$refs.xTable2) {
+            this.$refs.xTable2.loadData(tableData)
+          }
+          resolve()
+          this.loading = false
+        }, 300)
+      })
+    }
+  }
+}
+</script>
