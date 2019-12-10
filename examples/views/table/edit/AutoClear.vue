49ddc921d078f246fbfaffd942b75658c1de73c3
xuliangzhan <xu_liangzhan@163.com>
2019-12-10 13:50:22 +0800

    更新文档

--- examples/views/table/edit/AutoClear.vue
+++ examples/views/table/edit/AutoClear.vue
@@ -10,7 +10,7 @@
       :edit-config="{ trigger: 'manual', mode: 'row', autoClear: false}">
       <vxe-table-column type="index" width="60"></vxe-table-column>
       <vxe-table-column field="name" title="Name" :edit-render="{name: 'input'}"></vxe-table-column>
-      <vxe-table-column field="sex" title="Sex" :edit-render="{name: 'input'}"></vxe-table-column>
+      <vxe-table-column field="sex" title="Sex" :edit-render="{name: 'select', options: sexList}"></vxe-table-column>
       <vxe-table-column field="date" title="Date" :edit-render="{name: 'input'}"></vxe-table-column>
       <vxe-table-column field="address" title="Address" :edit-render="{name: 'input'}"></vxe-table-column>
       <vxe-table-column title="操作">
@@ -42,6 +42,7 @@ export default {
   data () {
     return {
       tableData: [],
+      sexList: [],
       demoCodes: [
         `
         <vxe-table
@@ -52,7 +53,7 @@ export default {
           :edit-config="{ trigger: 'manual', mode: 'row', autoClear: false}">
           <vxe-table-column type="index" width="60"></vxe-table-column>
           <vxe-table-column field="name" title="Name" :edit-render="{name: 'input'}"></vxe-table-column>
-          <vxe-table-column field="sex" title="Sex" :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="sex" title="Sex" :edit-render="{name: 'select', options: sexList}"></vxe-table-column>
           <vxe-table-column field="date" title="Date" :edit-render="{name: 'input'}"></vxe-table-column>
           <vxe-table-column field="address" title="Address" :edit-render="{name: 'input'}"></vxe-table-column>
           <vxe-table-column title="操作">
@@ -72,13 +73,20 @@ export default {
         export default {
           data () {
             return {
-              tableData: []
+              tableData: [],
+              sexList: []
             }
           },
           created () {
             this.tableData = window.MOCK_DATA_LIST.slice(0, 6)
+            this.findSexList()
           },
           methods: {
+            findSexList () {
+              return this.$ajax.get('/api/conf/sex/list').then(data => {
+                this.sexList = data
+              })
+            },
             editRowEvent (row) {
               this.$refs.xTable.setActiveRow(row)
             },
@@ -100,6 +108,7 @@ export default {
   },
   created () {
     this.tableData = window.MOCK_DATA_LIST.slice(0, 6)
+    this.findSexList()
   },
   mounted () {
     Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
@@ -107,6 +116,11 @@ export default {
     })
   },
   methods: {
+    findSexList () {
+      return this.$ajax.get('/api/conf/sex/list').then(data => {
+        this.sexList = data
+      })
+    },
     editRowEvent (row) {
       this.$refs.xTable.setActiveRow(row)
     },
