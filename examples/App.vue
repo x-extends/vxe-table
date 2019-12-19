155d665bbe3566b8a52ec1e585252bfc2a54aaf5
xuliangzhan <xu_liangzhan@163.com>
2019-12-19 12:46:18 +0800

    更新文档

--- examples/App.vue
+++ examples/App.vue
@@ -728,6 +728,7 @@ export default {
             },
             {
               label: 'app.aside.nav.filter',
+              demoUrl: 'https://jsrun.pro/CDWKp/edit',
               locat: {
                 name: 'TableTreeFilter'
               }
