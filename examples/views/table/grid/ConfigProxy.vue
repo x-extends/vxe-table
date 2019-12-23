598e6a7e7e9a43e65debeef13e1acb3e778df37f
xuliangzhan <xu_liangzhan@163.com>
2019-12-23 18:34:13 +0800

    更新文档

--- examples/views/table/grid/ConfigProxy.vue
+++ examples/views/table/grid/ConfigProxy.vue
@@ -97,77 +97,83 @@ export default {
       ],
       demoCodes: [
         `
-        <vxe-grid
-          resizable
-          height="528"
-          :filter-config="{remote: true}"
-          :toolbar="tableToolbar"
-          :pager-config="tablePage"
-          :columns="tableColumn"
-          :proxy-config="tableProxy"
-          :checkbox-config="{labelField: 'id', highlight: true, range: true}"
-          :edit-rules="validRules"
-          :edit-config="{trigger: 'click', mode: 'row', showStatus: true}">
-        </vxe-grid>
+        <vxe-grid v-bind="xGridOptions"></vxe-grid>
         `,
         `
         export default {
           data () {
             return {
-              validRules: {
-                name: [
-                  { required: true, message: '名称必须填写' },
-                  { min: 3, max: 50, message: '名称长度在 3 到 50 个字符' }
-                ],
-                role: [
-                  { required: true, message: '角色必须填写' }
-                ]
-              },
-              tablePage: {
-                pageSize: 15,
-                pageSizes: [5, 10, 20, 50, 100, 200, 500, 1000]
-              },
-              tableProxy: {
-                index: true,
-                sort: true,
-                filter: true,
-                ajax: {
-                  query: '/api/user/page/list/{{page.pageSize}}/{{page.currentPage}}',
-                  delete: '/api/user/save',
-                  save: '/api/user/save'
-                }
-              },
-              tableToolbar: {
-                buttons: [
-                  { code: 'insert_actived', name: 'Add', icon: 'fa fa-plus' },
-                  { code: 'mark_cancel', name: 'Mark', icon: 'fa fa-bookmark-o' },
-                  { code: 'save', name: 'Save', icon: 'fa fa-save' }
-                ],
-                refresh: true,
-                custom: true
-              },
-              tableColumn: [
-                { type: 'seq', width: 60, fixed: 'left' },
-                { type: 'checkbox', title: 'ID', width: 140, fixed: 'left' },
-                { field: 'name', title: 'Name', remoteSort: true, editRender: { name: 'input' } },
-                { field: 'nickname', title: 'Nickname', remoteSort: true, editRender: { name: 'input' } },
-                { field: 'age', title: 'Age', remoteSort: true, editRender: { name: 'input' } },
-                {
-                  field: 'role',
-                  title: 'Role',
-                  remoteSort: true,
-                  width: 200,
-                  filters: [
-                    { label: '前端开发', value: '前端' },
-                    { label: '后端开发', value: '后端' },
-                    { label: '测试', value: '测试' },
-                    { label: '程序员鼓励师', value: '程序员鼓励师' }
-                  ],
-                  filterMultiple: false,
-                  editRender: { name: 'input' }
+              xGridOptions: {
+                resizable: true,
+                height: 528,
+                filterConfig: {
+                  remote: true
                 },
-                { field: 'describe', title: 'Describe', showOverflow: true, editRender: { name: 'input' } }
-              ]
+                checkboxConfig: {
+                  labelField: 'id',
+                  highlight: true,
+                  range: true
+                },
+                editConfig: {
+                  trigger: 'click',
+                  mode: 'row',
+                  showStatus: true
+                },
+                editRules: {
+                  name: [
+                    { required: true, message: '名称必须填写' },
+                    { min: 3, max: 50, message: '名称长度在 3 到 50 个字符' }
+                  ],
+                  role: [
+                    { required: true, message: '角色必须填写' }
+                  ]
+                },
+                pagerConfig: {
+                  pageSize: 15,
+                  pageSizes: [5, 10, 20, 50, 100, 200, 500, 1000]
+                },
+                proxyConfig: {
+                  index: true,
+                  sort: true,
+                  filter: true,
+                  ajax: {
+                    query: '/api/user/page/list/{{page.pageSize}}/{{page.currentPage}}',
+                    delete: '/api/user/save',
+                    save: '/api/user/save'
+                  }
+                },
+                toolbar: {
+                  buttons: [
+                    { code: 'insert_actived', name: 'Add', icon: 'fa fa-plus' },
+                    { code: 'mark_cancel', name: 'Mark', icon: 'fa fa-bookmark-o' },
+                    { code: 'save', name: 'Save', icon: 'fa fa-save' }
+                  ],
+                  refresh: true,
+                  custom: true
+                },
+                columns: [
+                  { type: 'seq', width: 60, fixed: 'left' },
+                  { type: 'checkbox', title: 'ID', width: 140, fixed: 'left' },
+                  { field: 'name', title: 'Name', remoteSort: true, editRender: { name: 'input' } },
+                  { field: 'nickname', title: 'Nickname', remoteSort: true, editRender: { name: 'input' } },
+                  { field: 'age', title: 'Age', remoteSort: true, editRender: { name: 'input' } },
+                  {
+                    field: 'role',
+                    title: 'Role',
+                    remoteSort: true,
+                    width: 200,
+                    filters: [
+                      { label: '前端开发', value: '前端' },
+                      { label: '后端开发', value: '后端' },
+                      { label: '测试', value: '测试' },
+                      { label: '程序员鼓励师', value: '程序员鼓励师' }
+                    ],
+                    filterMultiple: false,
+                    editRender: { name: 'input' }
+                  },
+                  { field: 'describe', title: 'Describe', showOverflow: true, editRender: { name: 'input' } }
+                ]
+              }
             }
           }
         }
