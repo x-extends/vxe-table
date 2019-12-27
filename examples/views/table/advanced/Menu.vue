72c914b5ffd8ee2eb4952b2b6ada4489bd538a88
xuliangzhan <xu_liangzhan@163.com>
2019-12-27 17:18:22 +0800

    更新文档

--- examples/views/table/advanced/Menu.vue
+++ examples/views/table/advanced/Menu.vue
@@ -15,38 +15,6 @@
       @cell-context-menu="cellContextMenuEvent"
       @context-menu-click="contextMenuClickEvent">
       <vxe-table-column type="seq" width="60"></vxe-table-column>
-      <vxe-table-column type="expand" width="80">
-        <template v-slot:content="{ row, rowIndex }">
-          <template v-if="rowIndex === 1">
-            <vxe-table
-              border
-              :data="tableData">
-              <vxe-table-column field="role" title="Role"></vxe-table-column>
-              <vxe-table-column field="age" title="Age"></vxe-table-column>
-            </vxe-table>
-          </template>
-          <template v-else>
-            <ul>
-              <li>
-                <span>ID：</span>
-                <span>{{ row.id }}</span>
-              </li>
-              <li>
-                <span>Name：</span>
-                <span>{{ row.name }}</span>
-              </li>
-              <li>
-                <span>UpdateTime：</span>
-                <span>{{ row.updateTime }}</span>
-              </li>
-              <li>
-                <span>CreateTime：</span>
-                <span>{{ row.createTime }}</span>
-              </li>
-            </ul>
-          </template>
-        </template>
-      </vxe-table-column>
       <vxe-table-column field="name" title="Name" sortable></vxe-table-column>
       <vxe-table-column field="sex" title="Sex"></vxe-table-column>
       <vxe-table-column field="age" title="Age"></vxe-table-column>
