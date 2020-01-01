5f2c7bc9a95a8b29fb3ee873f4070c31261b5f4a
xuliangzhan <xu_liangzhan@163.com>
2020-01-01 17:08:03 +0800

    优化渲染

--- packages/cell/src/cell.js
+++ packages/cell/src/cell.js
@@ -115,7 +115,7 @@ export const Cell = {
     }
     return [
       h('div', {
-        class: ['vxe-cell--tree-none', {
+        class: ['vxe-cell--tree-node', {
           'is--active': isAceived
         }],
         style: {
