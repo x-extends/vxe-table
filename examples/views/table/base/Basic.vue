5aca3e426ce4c75a997c517ee00592050ca64177
xuliangzhan <xu_liangzhan@163.com>
2020-01-02 18:45:48 +0800

    更新文档

--- examples/views/table/base/Basic.vue
+++ examples/views/table/base/Basic.vue
@@ -2,18 +2,50 @@
   <div>
     <p class="tip"><table-api-link name="vxe-table"/> 基础表格，基于模板方式使用简单、便捷</p>
 
+    <vxe-toolbar>
+      <template v-slot:buttons>
+        <vxe-button @click="allAlign = 'left'">居左</vxe-button>
+        <vxe-button @click="allAlign = 'center'">居中</vxe-button>
+        <vxe-button @click="allAlign = 'right'">居右</vxe-button>
+      </template>
+    </vxe-toolbar>
+
     <vxe-table
-        border
-        highlight-hover-row
-        height="400"
-        :data="tableData">
-        <vxe-table-column type="checkbox" width="60"></vxe-table-column>
-        <vxe-table-column field="name" title="Name"></vxe-table-column>
-        <vxe-table-column field="sex" title="Sex" :filters="[{label: 'Man', value: '1'}, {label: 'Woman', value: '0'}]"></vxe-table-column>
-        <vxe-table-column field="date" title="Date"></vxe-table-column>
-        <vxe-table-column field="time" title="Time"></vxe-table-column>
-        <vxe-table-column field="address" title="Address"></vxe-table-column>
-      </vxe-table>
+      border="none"
+      :align="allAlign"
+      :data="tableData">
+      <vxe-table-column type="seq" width="60"></vxe-table-column>
+      <vxe-table-column field="name" title="app.body.label.name"></vxe-table-column>
+      <vxe-table-column field="sex" title="app.body.label.sex"></vxe-table-column>
+      <vxe-table-column field="age" title="app.body.label.age"></vxe-table-column>
+    </vxe-table>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[0] }}</code>
+      <code class="javascript">{{ demoCodes[1] }}</code>
+    </pre>
+
+    <p class="tip">使用 <table-api-link prop="highlight-hover-row"/> 属性启用 hover 行高亮</p>
+
+    <vxe-table
+      border
+      highlight-hover-row
+      :data="tableData">
+      <vxe-table-column type="seq" title="序号" width="60"></vxe-table-column>
+      <vxe-table-column field="name" title="Name"></vxe-table-column>
+      <vxe-table-column field="sex" title="Sex"></vxe-table-column>
+      <vxe-table-column field="age" title="Age"></vxe-table-column>
+      <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column>
+    </vxe-table>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[2] }}</code>
+      <code class="javascript">{{ demoCodes[3] }}</code>
+    </pre>
   </div>
 </template>
 
@@ -93,11 +125,6 @@ export default {
     Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
       hljs.highlightBlock(block)
     })
-  },
-  methods: {
-    filterMethod ({ value, row, column }) {
-      return row.id >= value
-    }
   }
 }
 </script>
