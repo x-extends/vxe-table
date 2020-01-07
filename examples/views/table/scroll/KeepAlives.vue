06051a4ad32afa2094738df9fef3495ea80827a6
xuliangzhan <xu_liangzhan@163.com>
2020-01-07 21:43:46 +0800

    更新文档

--- examples/views/table/scroll/KeepAlives.vue
+++ examples/views/table/scroll/KeepAlives.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <p class="tip">keep-alive 切换，v-show 可以使用 <table-api-link prop="auto-resize"/> 或者 <table-api-link prop="sync-resize"/> 自动监听响应式宽，也可以手动调用 <table-api-link prop="recalculate"/> 重新计算宽度</p>
+    <p class="tip">
+      keep-alive 切换<span class="red">（如果需要将表格放到隐藏的元素中，那么就必然会导致宽度无法计算）</span>，有以下方法解决<br>
+      1.在 actived 显示的事件中手动调用 <table-api-link prop="recalculate"/> 重新计算宽度 2.使用 <table-api-link prop="sync-resize"/> 绑定可视的变量实现自动更新宽度 3.使用 <table-api-link prop="auto-resize"/> 自动监听响应式更新宽度
+    </p>
 
     <p class="btns">
       <router-link class="link" :to="{name: 'TableScrollKeepAliveTable1'}">表格1</router-link>
