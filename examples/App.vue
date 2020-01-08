1e23ada66065053a7baea3c7dbe3e170dc82ec68
xuliangzhan <xu_liangzhan@163.com>
2020-01-08 16:08:03 +0800

    更新文档

--- examples/App.vue
+++ examples/App.vue
@@ -766,6 +766,12 @@ export default {
               }
             },
             {
+              label: 'app.aside.nav.treeSort',
+              locat: {
+                name: 'TableTreeSort'
+              }
+            },
+            {
               label: 'app.aside.nav.groupSummary',
               demoUrl: 'https://jsrun.pro/KVWKp/edit',
               locat: {
