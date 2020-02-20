ef846c3dbfe8a3e285ddf7cd933a92ec2e733fe4
xuliangzhan <xu_liangzhan@163.com>
2020-02-20 11:00:13 +0800

    更新文档

--- examples/App.vue
+++ examples/App.vue
@@ -1911,6 +1911,31 @@ export default {
       }
       return null
     },
+    showBetaVetsion () {
+      const { betaVersionList, stableVersionList } = this
+      if (betaVersionList.length && stableVersionList.length) {
+        const stableNums = stableVersionList[0].version.split('-')[0].split('.')
+        const stable1 = XEUtils.toNumber(stableNums[0])
+        const stable2 = XEUtils.toNumber(stableNums[1])
+        const stable3 = XEUtils.toNumber(stableNums[2])
+        const betaNums = betaVersionList[0].version.split('-')[0].split('.')
+        const beta1 = XEUtils.toNumber(betaNums[0])
+        const beta2 = XEUtils.toNumber(betaNums[1])
+        const beta3 = XEUtils.toNumber(betaNums[2])
+        if (beta1 > stable1) {
+          return true
+        } else if (beta1 === stable1) {
+          if (beta2 > stable2) {
+            return true
+          } else if (beta2 === stable2) {
+            if (beta3 > stable3) {
+              return true
+            }
+          }
+        }
+      }
+      return false
+    },
     newBetsVersionList () {
       const { betaVersionList, stableVersionList } = this
       if (betaVersionList.length && stableVersionList.length) {
@@ -1938,31 +1963,6 @@ export default {
         })
       }
       return []
-    },
-    showBetaVetsion () {
-      const { betaVersionList, stableVersionList } = this
-      if (betaVersionList.length && stableVersionList.length) {
-        const stableNums = stableVersionList[0].version.split('-')[0].split('.')
-        const stable1 = XEUtils.toNumber(stableNums[0])
-        const stable2 = XEUtils.toNumber(stableNums[1])
-        const stable3 = XEUtils.toNumber(stableNums[2])
-        const betaNums = betaVersionList[0].version.split('-')[0].split('.')
-        const beta1 = XEUtils.toNumber(betaNums[0])
-        const beta2 = XEUtils.toNumber(betaNums[1])
-        const beta3 = XEUtils.toNumber(betaNums[2])
-        if (beta1 > stable1) {
-          return true
-        } else if (beta1 === stable1) {
-          if (beta2 > stable2) {
-            return true
-          } else if (beta2 === stable2) {
-            if (beta3 > stable3) {
-              return true
-            }
-          }
-        }
-      }
-      return false
     },
     pageKey () {
       return this.$route.path.split('/')[2]
