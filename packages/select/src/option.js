42e51fe9408ac5a965c5ce5906f394dbb98e5bb6
xuliangzhan <xu_liangzhan@163.com>
2020-02-25 17:10:51 +0800

    优化重构

---
+++ packages/select/src/option.js
@@ -0,0 +1,74 @@
+import { UtilTools } from '../../tools'
+
+let optionUniqueId = 0
+
+export default {
+  name: 'VxeOption',
+  props: {
+    value: [String, Number],
+    label: [String, Number],
+    disabled: Boolean,
+    size: String
+  },
+  inject: {
+    $xeselect: {
+      default: null
+    },
+    $xeoptgroup: {
+      default: null
+    }
+  },
+  data () {
+    return {
+      id: `option_${++optionUniqueId}`
+    }
+  },
+  computed: {
+    vSize () {
+      return this.size || this.$parent.size || this.$parent.vSize
+    },
+    isDisabled () {
+      const { $xeoptgroup, disabled } = this
+      return ($xeoptgroup && $xeoptgroup.disabled) || disabled
+    }
+  },
+  warch: {
+    value () {
+      this.updateView()
+    }
+  },
+  mounted () {
+    this.updateView()
+  },
+  destroyed () {
+    this.updateView()
+  },
+  render (h) {
+    const { $xeselect, id, isDisabled, value } = this
+    return h('div', {
+      class: ['vxe-select-option', {
+        'is--disabled': isDisabled,
+        'is--checked': $xeselect.value === value,
+        'is--hover': $xeselect.currentValue === value
+      }],
+      attrs: {
+        'data-option-id': id
+      },
+      on: {
+        click: this.optionEvent,
+        mouseenter: this.mouseenterEvent
+      }
+    }, UtilTools.getFuncText(this.label))
+  },
+  methods: {
+    updateView () {
+      this.$xeselect.updateStatus()
+    },
+    optionEvent (evnt) {
+      this.$xeselect.changeOptionEvent(evnt, this.value)
+    },
+    mouseenterEvent () {
+      this.$xeselect.setCurrentOption(this)
+    }
+  }
+}
