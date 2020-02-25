499e9228690b8dc05ccd704b73cfd8e0115470b6
xuliangzhan <xu_liangzhan@163.com>
2020-02-25 16:20:22 +0800

    优化重构

---
+++ types/select.d.ts
@@ -0,0 +1,13 @@
+import { VXETableModule } from './module';
+
+/**
+ * 分页
+ */
+export declare class Select extends VXETableModule {
+  /**
+   * 大小
+   * @default 'default'
+   * @type string
+   */
+  size: 'medium' | 'small' | 'mini';
+}
