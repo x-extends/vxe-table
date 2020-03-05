7987d5848ad48f23a2f76a83abc5a5393594f5d8
xuliangzhan <xu_liangzhan@163.com>
2020-03-06 00:23:29 +0800

    更新文档

--- examples/views/table/other/Xlsx.vue
+++ examples/views/table/other/Xlsx.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="tip">
       使用 <a class="link" href="https://www.npmjs.com/package/xlsx" target="_blank">xlsx</a> 实现导入数据<br>
-      <span class="red">（注：该示例仅供参考，具体逻辑需要自行实现）</span>
+      <span class="red">（注：该示例仅供参考，具体逻辑请自行实现）</span>
     </p>
 
     <vxe-toolbar>
