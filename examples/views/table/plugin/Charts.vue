3a1462ac2762f8047eb97b14335e31b96dfe1e84
xuliangzhan <xu_liangzhan@163.com>
2020-03-06 18:16:01 +0800

    更新文档

--- examples/views/table/plugin/Charts.vue
+++ examples/views/table/plugin/Charts.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="tip">
       具体兼容请查看 <a class="link" href="https://www.npmjs.com/package/vxe-table-plugin-charts" target="_blank">vxe-table-plugin-charts</a> 插件的 API<br>
-      <span class="red">（注：仅支持简单的图表功能）</span>
+      <span class="red">（注：实验功能！仅支持简单的图表功能，不建议用于生产）</span>
     </p>
 
     <vxe-table
