8adab96b5f801509cd8cb6021f4ba1ccef7ef86e
xuliangzhan <xu_liangzhan@163.com>
2020-03-08 15:10:02 +0800

    优化键盘导航

--- examples/views/table/base/AutoHeight.vue
+++ examples/views/table/base/AutoHeight.vue
@@ -6,7 +6,7 @@
       还可以根据不同场景添加 <table-api-link prop="sync-resize"/>（属性监听） 或 <table-api-link prop="auto-resize"/>（父元素监听），这样就只需要通过样式控制父容器高度就可以实现响应式表格
     </p>
 
-    <div style="height: 600px">
+    <div style="height: 300px">
       <vxe-table
         border
         height="auto"
@@ -26,9 +26,9 @@
       <code class="javascript">{{ demoCodes[1] }}</code>
     </pre>
 
-    <p>也可以设置相对于父容器的百分比</p>
+    <p class="tip">也可以设置相对于父容器的百分比</p>
 
-    <div style="height: 600px">
+    <div style="height: 500px">
       <vxe-table
         border
         height="40%"
@@ -57,6 +57,37 @@
       <code class="xml">{{ demoCodes[2] }}</code>
       <code class="javascript">{{ demoCodes[3] }}</code>
     </pre>
+
+    <p class="tip">设置为 <table-api-link prop="auto-resize"/> 响应式就可以自动跟随父容器高度</p>
+
+    <vxe-toolbar>
+      <template v-slot:buttons>
+        <vxe-button @click="tableHeight = 200">修改200px</vxe-button>
+        <vxe-button @click="tableHeight = 400">修改400px</vxe-button>
+        <vxe-button @click="tableHeight = 600">修改600px</vxe-button>
+      </template>
+    </vxe-toolbar>
+
+    <div :style="{height: `${tableHeight}px`}">
+      <vxe-table
+        border
+        auto-resize
+        height="auto"
+        :data="tableData">
+        <vxe-table-column type="seq" width="60"></vxe-table-column>
+        <vxe-table-column field="name" title="Name"></vxe-table-column>
+        <vxe-table-column field="sex" title="Sex"></vxe-table-column>
+        <vxe-table-column field="age" title="Age"></vxe-table-column>
+        <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column>
+      </vxe-table>
+    </div>
+
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[4] }}</code>
+      <code class="javascript">{{ demoCodes[5] }}</code>
+    </pre>
   </div>
 </template>
 
@@ -66,6 +97,7 @@ import hljs from 'highlight.js'
 export default {
   data () {
     return {
+      tableHeight: 200,
       tableData: [],
       demoCodes: [
         `
@@ -122,6 +154,42 @@ export default {
         export default {
           data () {
             return {
+              tableData: []
+            }
+          },
+          created () {
+            this.tableData = window.MOCK_DATA_LIST.slice(0, 20)
+          }
+        }
+        `,
+        `
+        <vxe-toolbar>
+          <template v-slot:buttons>
+            <vxe-button @click="tableHeight = 200">修改200px</vxe-button>
+            <vxe-button @click="tableHeight = 400">修改400px</vxe-button>
+            <vxe-button @click="tableHeight = 600">修改600px</vxe-button>
+          </template>
+        </vxe-toolbar>
+
+        <div :style="{height: \`\${tableHeight}px\`}">
+          <vxe-table
+            border
+            auto-resize
+            height="auto"
+            :data="tableData">
+            <vxe-table-column type="seq" width="60"></vxe-table-column>
+            <vxe-table-column field="name" title="Name"></vxe-table-column>
+            <vxe-table-column field="sex" title="Sex"></vxe-table-column>
+            <vxe-table-column field="age" title="Age"></vxe-table-column>
+            <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column>
+          </vxe-table>
+        </div>
+        `,
+        `
+        export default {
+          data () {
+            return {
+              tableHeight: 200,
               tableData: []
             }
           },
