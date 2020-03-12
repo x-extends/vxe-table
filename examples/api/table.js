4188d34f1cc2416828209b1be67e330114b636ee
xuliangzhan <xu_liangzhan@163.com>
2020-03-12 16:47:16 +0800

    更新文档

--- examples/api/table.js
+++ examples/api/table.js
@@ -3710,7 +3710,7 @@ const apis = [
       },
       {
         name: 'updateFooter()',
-        desc: '手动更新表尾合计（对于某些需要频繁更新的场景下可能会用到）',
+        desc: '手动更新表尾（对于某些需要频繁更新的场景下可能会用到）',
         version: '',
         type: 'Promise',
         enum: '',
