89267d24ddf9c47ac196aaebddfd2081d35ba685
xuliangzhan <xu_liangzhan@163.com>
2020-03-14 19:04:10 +0800

    更新文档

--- examples/i18n/index.js
+++ examples/i18n/index.js
@@ -3,27 +3,38 @@ import VueI18n from 'vue-i18n'
 
 import tableEnUS from '../../packages/locale/lang/en-US'
 import tableZhCN from '../../packages/locale/lang/zh-CN'
-import elementEn from 'element-ui/lib/locale/lang/en'
-import elementZhCN from 'element-ui/lib/locale/lang/zh-CN'
+import tableZhTW from '../../packages/locale/lang/zh-TW'
+import tableJAJP from '../../packages/locale/lang/ja-JP'
 
 import myEnUS from './lang/en-US'
 import myZhCN from './lang/zh-CN'
+import myZhTW from './lang/zh-TW'
+import myJAJP from './lang/ja-JP'
 
 Vue.use(VueI18n)
 
+const languageList = ['zh_CN', 'zh_TW', 'zh_TW', 'ja_JP']
+const customLanguage = localStorage.getItem('language')
+
 const i18n = new VueI18n({
   /* eslint-disable @typescript-eslint/camelcase */
-  locale: ['zh-CN', 'zh-TW'].includes(navigator.language) ? 'zh_CN' : 'en_US',
+  locale: customLanguage && languageList.includes(customLanguage) ? customLanguage : ['zh-CN', 'zh-TW'].includes(navigator.language) ? 'zh_CN' : 'en_US',
   messages: {
     en_US: {
       ...tableEnUS,
-      ...elementEn,
       ...myEnUS
     },
     zh_CN: {
       ...tableZhCN,
-      ...elementZhCN,
       ...myZhCN
+    },
+    zh_TW: {
+      ...tableZhTW,
+      ...myZhTW
+    },
+    ja_JP: {
+      ...tableJAJP,
+      ...myJAJP
     }
   }
 })
