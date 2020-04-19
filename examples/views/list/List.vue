debad384e1717b71eb45b1f3ac235dacca1c5150
xuliangzhan <xu_liangzhan@163.com>
2020-04-19 20:29:09 +0800

    修复已知问题

--- examples/views/list/List.vue
+++ examples/views/list/List.vue
@@ -70,6 +70,33 @@
       </div>
     </div>
 
+    <p class="tip">实现高性能的虚拟树列表：1.拍平树结构 2.构建列表树结构 3.处理展开收缩</p>
+
+    <p>
+      <vxe-button @click="loadTree6(10)">加载2000条</vxe-button>
+      <vxe-button @click="loadTree6(45)">加载2w条</vxe-button>
+      <vxe-button @click="loadTree6(100)">加载5w条</vxe-button>
+      <vxe-button @click="loadTree6(200)">加载10w条</vxe-button>
+      <vxe-button @click="allTreeExpand(true)">全部展开</vxe-button>
+      <vxe-button @click="allTreeExpand(false)">全部收起</vxe-button>
+    </p>
+
+    <p>
+      <vxe-list height="240" class="my-tree" :loading="loading6" :data="list6">
+        <template v-slot="{ items }">
+          <div
+            class="my-tree-item"
+            v-for="(item, index) in items"
+            :key="index"
+            :class="[`level-${item._LEVEL}`, {'has-child': item._HAS_CHILDREN}]"
+            :style="{paddingLeft: `${item._LEVEL * 20}px`}">
+            <i class="tree-icon fa" :class="[item._EXPAND ? 'fa-chevron-right' : 'fa-chevron-down']" @click="toggleTreeNode(item)"></i>
+            <span class="tree-label">{{ item.label }}</span>
+          </div>
+        </template>
+      </vxe-list>
+    </p>
+
     <pre>
       <code>
         | Arrow Up ↑ | 匀速向上滚动数据 |
@@ -96,6 +123,7 @@
 
 <script>
 import hljs from 'highlight.js'
+import XEUtils from 'xe-utils'
 
 const mockList = []
 
@@ -103,11 +131,13 @@ export default {
   data () {
     return {
       loading1: false,
+      loading6: false,
       list1: [],
       list2: [],
       list3: [],
       list4: [],
       list5: [],
+      list6: [],
       demoCodes: [
         `
         <p>
@@ -173,21 +203,49 @@ export default {
             </vxe-list>
           </div>
         </div>
+
+        <p>
+          <vxe-button @click="loadTree6(10)">加载2000条</vxe-button>
+          <vxe-button @click="loadTree6(45)">加载2w条</vxe-button>
+          <vxe-button @click="loadTree6(100)">加载5w条</vxe-button>
+          <vxe-button @click="loadTree6(200)">加载10w条</vxe-button>
+          <vxe-button @click="allTreeExpand(true)">全部展开</vxe-button>
+          <vxe-button @click="allTreeExpand(false)">全部收起</vxe-button>
+        </p>
+
+        <p>
+          <vxe-list height="240" class="my-tree" :loading="loading6" :data="list6">
+            <template v-slot="{ items }">
+              <div
+                class="my-tree-item"
+                v-for="(item, index) in items"
+                :key="index"
+                :class="[\`level-\${item._LEVEL}\`, {'has-child': item._HAS_CHILDREN}]"
+                :style="{paddingLeft: \`\${item._LEVEL * 20}px\`}">
+                <i class="tree-icon fa" :class="[item._EXPAND ? 'fa-chevron-right' : 'fa-chevron-down']" @click="toggleTreeNode(item)"></i>
+                <span class="tree-label">{{ item.label }}</span>
+              </div>
+            </template>
+          </vxe-list>
+        </p>
         `,
         `
         export default {
           data () {
             return {
               loading1: false,
+              loading6: false,
               list1: [],
               list2: [],
               list3: [],
               list4: [],
-              list5: []
+              list5: [],
+              list6: []
             }
           },
           created () {
             this.loadData1(500)
+            this.loadTree6(10)
             this.list2 = this.getList(40)
             this.list3 = this.getList(400)
             this.list4 = this.getList(2000)
@@ -205,6 +263,91 @@ export default {
               }
               return mockList.slice(0, size)
             },
+            getTree (size) {
+              const result = []
+              for (let index = 0; index < size; index++) {
+                const item = {
+                  label: \`节点 \${index}\`
+                }
+                if (index) {
+                  if (index % 33 === 0) {
+                    const childList = []
+                    for (let cIndex = 0; cIndex < 1000; cIndex++) {
+                      childList.push({
+                        label: \`子节点 \${index}-\${cIndex}\`,
+                        children: [
+                          { label: \`子节点 \${index}-\${cIndex}-0\` },
+                          { label: \`子节点 \${index}-\${cIndex}-1\` },
+                          { label: \`子节点 \${index}-\${cIndex}-2\` },
+                          { label: \`子节点 \${index}-\${cIndex}-3\` },
+                          { label: \`子节点 \${index}-\${cIndex}-4\` },
+                          { label: \`子节点 \${index}-\${cIndex}-5\` },
+                          { label: \`子节点 \${index}-\${cIndex}-6\` },
+                          { label: \`子节点 \${index}-\${cIndex}-7\` }
+                        ]
+                      })
+                    }
+                    item.children = childList
+                  } else if (index % 22 === 0) {
+                    const childList = []
+                    for (let cIndex = 0; cIndex < 500; cIndex++) {
+                      childList.push({
+                        label: \`子节点 \${index}-\${cIndex}\`,
+                        children: [
+                          { label: \`子节点 \${index}-\${cIndex}-0\` },
+                          { label: \`子节点 \${index}-\${cIndex}-1\` },
+                          { label: \`子节点 \${index}-\${cIndex}-2\` }
+                        ]
+                      })
+                    }
+                    item.children = childList
+                  } else if (index % 9 === 0) {
+                    const childList = []
+                    for (let cIndex = 0; cIndex < 200; cIndex++) {
+                      childList.push({
+                        label: \`子节点 \${index}-\${cIndex}\`,
+                        children: [
+                          { label: \`子节点 \${index}-\${cIndex}-0\` },
+                          { label: \`子节点 \${index}-\${cIndex}-1\` },
+                          { label: \`子节点 \${index}-\${cIndex}-2\` },
+                          { label: \`子节点 \${index}-\${cIndex}-4\` },
+                          { label: \`子节点 \${index}-\${cIndex}-5\` }
+                        ]
+                      })
+                    }
+                    item.children = childList
+                  } else if (index % 6 === 0) {
+                    const childList = []
+                    for (let cIndex = 0; cIndex < 100; cIndex++) {
+                      childList.push({
+                        label: \`子节点 \${index}-\${cIndex}\`,
+                        children: [
+                          { label: \`子节点 \${index}-\${cIndex}-0\` },
+                          { label: \`子节点 \${index}-\${cIndex}-1\` },
+                          { label: \`子节点 \${index}-\${cIndex}-2\` },
+                          { label: \`子节点 \${index}-\${cIndex}-3\` }
+                        ]
+                      })
+                    }
+                    item.children = childList
+                  } else if (index % 3 === 0) {
+                    const childList = []
+                    for (let cIndex = 0; cIndex < 10; cIndex++) {
+                      childList.push({
+                        label: \`子节点 \${index}-\${cIndex}\`,
+                        children: [
+                          { label: \`子节点 \${index}-\${cIndex}-0\` },
+                          { label: \`子节点 \${index}-\${cIndex}-1\` }
+                        ]
+                      })
+                    }
+                    item.children = childList
+                  }
+                }
+                result.push(item)
+              }
+              return result
+            },
             loadData1 (size) {
               // 模拟后台
               this.loading1 = true
@@ -216,6 +359,73 @@ export default {
                   this.$XModal.message({ message: \`渲染 \${size} 行，用时 \${Date.now() - startTime}毫秒\`, status: 'info' })
                 })
               }, 200)
+            },
+            loadTree6 (size) {
+              // 模拟后台
+              this.loading6 = true
+              setTimeout(() => {
+                const trerData = this.getTree(size)
+                // 将树结构拍平，构建列表树结构
+                XEUtils.eachTree(trerData, (item, index, items, paths, parent, nodes) => {
+                  // 层级
+                  item._LEVEL = nodes.length - 1
+                  // 是否展开
+                  item._EXPAND = false
+                  // 是否可视
+                  item._VISIBLE = !item._LEVEL
+                  // 是否有子节点
+                  item._HAS_CHILDREN = item.children && item.children.length > 0
+                  // 是否叶子节点
+                  item._IS_LEAF = !item._HAS_CHILDREN
+                })
+                this.tree6 = trerData
+                this.refreshTree()
+                this.loading6 = false
+                const startTime = Date.now()
+                this.$nextTick(() => {
+                  this.$XModal.message({ message: \`渲染 \${this.fullList6.length} 行，用时 \${Date.now() - startTime}毫秒\`, status: 'info' })
+                })
+              }, 200)
+            },
+            toggleTreeNode (row) {
+              if (row._HAS_CHILDREN) {
+                this.setTreeExpand(row, !row._EXPAND)
+              }
+            },
+            setTreeExpand (row, isExpand) {
+              const matchObj = XEUtils.findTree(this.tree6, item => item === row)
+              row._EXPAND = isExpand
+              if (matchObj) {
+                if (isExpand) {
+                  matchObj.item.children.forEach(item => {
+                    item._VISIBLE = true
+                  })
+                } else {
+                  XEUtils.eachTree(matchObj.item.children, item => {
+                    item._VISIBLE = false
+                  })
+                }
+              }
+              this.refreshTree()
+            },
+            allTreeExpand (isExpand) {
+              if (isExpand) {
+                XEUtils.eachTree(this.tree6, item => {
+                  item._EXPAND = item._HAS_CHILDREN
+                  item._VISIBLE = true
+                })
+              } else {
+                XEUtils.eachTree(this.tree6, item => {
+                  item._EXPAND = false
+                  item._VISIBLE = !item._LEVEL
+                })
+              }
+              this.refreshTree()
+            },
+            refreshTree () {
+              const treeList = XEUtils.toTreeArray(this.tree6)
+              this.fullList6 = treeList
+              this.list6 = treeList.filter(item => item._VISIBLE)
             }
           }
         }
@@ -251,7 +461,24 @@ export default {
           background-color: #f5f7fa;
         }
         .my-table-list td {
-          border-right: 1px solid #e8eaec;;
+          border-right: 1px solid #e8eaec;
+        }
+        .my-tree {
+          padding: 0 15px;
+          border: 1px solid #e8eaec;
+        }
+        .my-tree .my-tree-item {
+          height: 32px;
+          line-height: 32px;
+        }
+        .my-tree .my-tree-item.has-child .tree-icon {
+          visibility: visible;
+        }
+        .my-tree .tree-icon {
+          cursor: pointer;
+          width: 20px;
+          visibility: hidden;
+          user-select: none;
         }
         `
       ]
@@ -259,6 +486,7 @@ export default {
   },
   created () {
     this.loadData1(500)
+    this.loadTree6(10)
     this.list2 = this.getList(40)
     this.list3 = this.getList(400)
     this.list4 = this.getList(2000)
@@ -281,6 +509,91 @@ export default {
       }
       return mockList.slice(0, size)
     },
+    getTree (size) {
+      const result = []
+      for (let index = 0; index < size; index++) {
+        const item = {
+          label: `节点 ${index}`
+        }
+        if (index) {
+          if (index % 33 === 0) {
+            const childList = []
+            for (let cIndex = 0; cIndex < 1000; cIndex++) {
+              childList.push({
+                label: `子节点 ${index}-${cIndex}`,
+                children: [
+                  { label: `子节点 ${index}-${cIndex}-0` },
+                  { label: `子节点 ${index}-${cIndex}-1` },
+                  { label: `子节点 ${index}-${cIndex}-2` },
+                  { label: `子节点 ${index}-${cIndex}-3` },
+                  { label: `子节点 ${index}-${cIndex}-4` },
+                  { label: `子节点 ${index}-${cIndex}-5` },
+                  { label: `子节点 ${index}-${cIndex}-6` },
+                  { label: `子节点 ${index}-${cIndex}-7` }
+                ]
+              })
+            }
+            item.children = childList
+          } else if (index % 22 === 0) {
+            const childList = []
+            for (let cIndex = 0; cIndex < 500; cIndex++) {
+              childList.push({
+                label: `子节点 ${index}-${cIndex}`,
+                children: [
+                  { label: `子节点 ${index}-${cIndex}-0` },
+                  { label: `子节点 ${index}-${cIndex}-1` },
+                  { label: `子节点 ${index}-${cIndex}-2` }
+                ]
+              })
+            }
+            item.children = childList
+          } else if (index % 9 === 0) {
+            const childList = []
+            for (let cIndex = 0; cIndex < 200; cIndex++) {
+              childList.push({
+                label: `子节点 ${index}-${cIndex}`,
+                children: [
+                  { label: `子节点 ${index}-${cIndex}-0` },
+                  { label: `子节点 ${index}-${cIndex}-1` },
+                  { label: `子节点 ${index}-${cIndex}-2` },
+                  { label: `子节点 ${index}-${cIndex}-4` },
+                  { label: `子节点 ${index}-${cIndex}-5` }
+                ]
+              })
+            }
+            item.children = childList
+          } else if (index % 6 === 0) {
+            const childList = []
+            for (let cIndex = 0; cIndex < 100; cIndex++) {
+              childList.push({
+                label: `子节点 ${index}-${cIndex}`,
+                children: [
+                  { label: `子节点 ${index}-${cIndex}-0` },
+                  { label: `子节点 ${index}-${cIndex}-1` },
+                  { label: `子节点 ${index}-${cIndex}-2` },
+                  { label: `子节点 ${index}-${cIndex}-3` }
+                ]
+              })
+            }
+            item.children = childList
+          } else if (index % 3 === 0) {
+            const childList = []
+            for (let cIndex = 0; cIndex < 10; cIndex++) {
+              childList.push({
+                label: `子节点 ${index}-${cIndex}`,
+                children: [
+                  { label: `子节点 ${index}-${cIndex}-0` },
+                  { label: `子节点 ${index}-${cIndex}-1` }
+                ]
+              })
+            }
+            item.children = childList
+          }
+        }
+        result.push(item)
+      }
+      return result
+    },
     loadData1 (size) {
       // 模拟后台
       this.loading1 = true
@@ -292,6 +605,73 @@ export default {
           this.$XModal.message({ message: `渲染 ${size} 行，用时 ${Date.now() - startTime}毫秒`, status: 'info' })
         })
       }, 200)
+    },
+    loadTree6 (size) {
+      // 模拟后台
+      this.loading6 = true
+      setTimeout(() => {
+        const trerData = this.getTree(size)
+        // 将树结构拍平，构建列表树结构
+        XEUtils.eachTree(trerData, (item, index, items, paths, parent, nodes) => {
+          // 层级
+          item._LEVEL = nodes.length - 1
+          // 是否展开
+          item._EXPAND = false
+          // 是否可视
+          item._VISIBLE = !item._LEVEL
+          // 是否有子节点
+          item._HAS_CHILDREN = item.children && item.children.length > 0
+          // 是否叶子节点
+          item._IS_LEAF = !item._HAS_CHILDREN
+        })
+        this.tree6 = trerData
+        this.refreshTree()
+        this.loading6 = false
+        const startTime = Date.now()
+        this.$nextTick(() => {
+          this.$XModal.message({ message: `渲染 ${this.fullList6.length} 行，用时 ${Date.now() - startTime}毫秒`, status: 'info' })
+        })
+      }, 200)
+    },
+    toggleTreeNode (row) {
+      if (row._HAS_CHILDREN) {
+        this.setTreeExpand(row, !row._EXPAND)
+      }
+    },
+    setTreeExpand (row, isExpand) {
+      const matchObj = XEUtils.findTree(this.tree6, item => item === row)
+      row._EXPAND = isExpand
+      if (matchObj) {
+        if (isExpand) {
+          matchObj.item.children.forEach(item => {
+            item._VISIBLE = true
+          })
+        } else {
+          XEUtils.eachTree(matchObj.item.children, item => {
+            item._VISIBLE = false
+          })
+        }
+      }
+      this.refreshTree()
+    },
+    allTreeExpand (isExpand) {
+      if (isExpand) {
+        XEUtils.eachTree(this.tree6, item => {
+          item._EXPAND = item._HAS_CHILDREN
+          item._VISIBLE = true
+        })
+      } else {
+        XEUtils.eachTree(this.tree6, item => {
+          item._EXPAND = false
+          item._VISIBLE = !item._LEVEL
+        })
+      }
+      this.refreshTree()
+    },
+    refreshTree () {
+      const treeList = XEUtils.toTreeArray(this.tree6)
+      this.fullList6 = treeList
+      this.list6 = treeList.filter(item => item._VISIBLE)
     }
   }
 }
@@ -328,6 +708,23 @@ export default {
   background-color: #f5f7fa;
 }
 .my-table-list td {
-  border-right: 1px solid #e8eaec;;
+  border-right: 1px solid #e8eaec;
+}
+.my-tree {
+  padding: 0 15px;
+  border: 1px solid #e8eaec;
+}
+.my-tree .my-tree-item {
+  height: 32px;
+  line-height: 32px;
+}
+.my-tree .my-tree-item.has-child .tree-icon {
+  visibility: visible;
+}
+.my-tree .tree-icon {
+  cursor: pointer;
+  width: 20px;
+  visibility: hidden;
+  user-select: none;
 }
 </style>
