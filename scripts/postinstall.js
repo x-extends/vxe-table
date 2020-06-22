da99958ddf6012ad4b84cae4ccc140aec057e649
xuliangzhan <xu_liangzhan@163.com>
2020-06-22 23:39:35 +0800

    增加 type=search

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,9 +8,7 @@ function is(it) {
 }
 
 if (!ADBLOCK && !CI && !DISABLE_OPENCOLLECTIVE) {
-  console.log('\n\x1B[94m>\x1B[94m 感谢使用 vxe-table，为了使项目能够健康持续的发展下去，您可以通过捐赠来支持作者： \x1B[0m');
-  console.log('\x1B[96m>\x1B[96m https://xuliangzhan_admin.gitee.io/vxe-table/#/donation/api \x1B[0m\n');
-
-  console.log('\x1B[94m>\x1B[94m Thanks for using vxe-table. In order to keep the project healthy and sustainable, you can support the author through donation: \x1B[0m');
-  console.log('\x1B[96m>\x1B[96m https://xuliangzhan.com/vxe-table/index.html#/donation/api \x1B[0m\n\n');
+  console.log('\n\x1B[89m>\x1B[93m ✌ 感谢使用 vxe-table ◕‿◕ \x1B[0m');
+  console.log('\x1B[89m>\x1B[93m 由于维护一个开源项目需要花费非常大的精力与时间，如果该项目对您有帮助，可以通过以下捐赠来帮助该项目能持续发展下去:  \x1B[0m');
+  console.log('\x1B[89m>\x1B[96m https://xuliangzhan_admin.gitee.io/vxe-table/#/donation/api \x1B[0m\n\n');
 }
