d8614941806a987fc343ca5facf740c5efa3c6b4
xuliangzhan <xu_liangzhan@163.com>
2020-08-19 21:13:33 +0800

    修复已知问题

--- packages/tools/src/event.js
+++ packages/tools/src/event.js
@@ -26,9 +26,11 @@ export const GlobalEvent = {
 }
 
 if (browse.isDoc) {
-  document.addEventListener('copy', GlobalEvent.trigger, false)
-  document.addEventListener('cut', GlobalEvent.trigger, false)
-  document.addEventListener('paste', GlobalEvent.trigger, false)
+  if (!browse.msie) {
+    document.addEventListener('copy', GlobalEvent.trigger, false)
+    document.addEventListener('cut', GlobalEvent.trigger, false)
+    document.addEventListener('paste', GlobalEvent.trigger, false)
+  }
   document.addEventListener('keydown', GlobalEvent.trigger, false)
   document.addEventListener('contextmenu', GlobalEvent.trigger, false)
   window.addEventListener('mousedown', GlobalEvent.trigger, false)
