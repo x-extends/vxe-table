f5cbd8b17d2b4eda4a6f26ea14d54912df26f553
xuliangzhan <xu_liangzhan@163.com>
2020-09-02 11:37:13 +0800

    update

--- examples/App.vue
+++ examples/App.vue
@@ -2405,8 +2405,9 @@ export default {
           location.href = '/vxe-table/v3/index.html'
           break
         case '4':
-          this.version = '2'
-          this.$XModal.message({ message: this.$t('app.body.other.newDevelopment'), status: 'info' })
+          this.$XModal.alert({ message: this.$t('app.body.other.newDevelopment'), status: 'info' }).then(() => {
+            location.href = '/vxe-table/'
+          })
           break
       }
     }
