0448fcde16ba384a766a07a1ce3e3df3b56f7714
xuliangzhan <xu_liangzhan@163.com>
2020-09-14 18:49:24 +0800

    update

--- examples/plugins/xtable/index.js
+++ examples/plugins/xtable/index.js
@@ -32,30 +32,18 @@ import {
   Resize
 } from '../../../packages/vxe-table'
 
-// import VXETablePluginElement from '../../../../vxe-table-plugin-element/test.js'
-// import VXETablePluginIView from '../../../../vxe-table-plugin-iview/test.js'
-// import VXETablePluginAntd from '../../../../vxe-table-plugin-antd/test.js'
-// import VXETablePluginExcel from '../../../../vxe-table-plugin-excel/test.js'
-// import VXETablePluginRenderer from '../../../../vxe-table-plugin-renderer/test.js'
-// import VXETablePluginMenus from '../../../../vxe-table-plugin-menus/test.js'
-// import VXETablePluginShortcutKey from '../../../../vxe-table-plugin-shortcut-key/test.js'
 import VXETablePluginElement from 'vxe-table-plugin-element'
 import VXETablePluginIView from 'vxe-table-plugin-iview'
 import VXETablePluginAntd from 'vxe-table-plugin-antd'
-import VXETablePluginExcel from 'vxe-table-plugin-excel'
-import VXETablePluginRenderer from 'vxe-table-plugin-renderer'
 import VXETablePluginVirtualTree from 'vxe-table-plugin-virtual-tree'
 import VXETablePluginMenus from 'vxe-table-plugin-menus'
 import VXETablePluginExportXLSX from 'vxe-table-plugin-export-xlsx'
 import VXETablePluginExportPDF from 'vxe-table-plugin-export-pdf'
-// import VXETablePluginShortcutKey from 'vxe-table-plugin-shortcut-key'
 import 'vxe-table-plugin-element/dist/style.css'
 import 'vxe-table-plugin-iview/dist/style.css'
 import 'vxe-table-plugin-antd/dist/style.css'
 import 'vxe-table-plugin-excel/dist/style.css'
 import 'vxe-table-plugin-virtual-tree/dist/style.css'
-import 'vxe-table-plugin-renderer/dist/style.css'
-import 'vxe-table-plugin-charts/dist/style.css'
 
 // 设置默认参数
 VXETable.setup({
@@ -104,42 +92,7 @@ Vue.prototype.$XModal = VXETable.modal
 VXETable.use(VXETablePluginElement)
 VXETable.use(VXETablePluginIView)
 VXETable.use(VXETablePluginAntd)
-VXETable.use(VXETablePluginExcel)
-VXETable.use(VXETablePluginRenderer)
 VXETable.use(VXETablePluginVirtualTree)
 VXETable.use(VXETablePluginMenus)
 VXETable.use(VXETablePluginExportXLSX)
 VXETable.use(VXETablePluginExportPDF)
-// VXETable.use(VXETablePluginShortcutKey, {
-//   disabled: ['ArrowLeft'],
-//   listener: {
-//     'V' (params, evnt) {
-//       console.log('粘贴')
-//     },
-//     'Shift + V' (params, evnt) {
-//       console.log('粘贴')
-//     },
-//     'Ctrl + V' (params, evnt) {
-//       console.log('粘贴')
-//     },
-//     'C' (params, evnt) {
-//       console.log('粘贴')
-//     }
-//   },
-//   setting: {
-//     'table.edit.actived': 'F2',
-//     'table.edit.closed': 'Esc',
-//     'table.edit.leftTabMove': 'Shift + Enter',
-//     'table.edit.rightTabMove': 'Enter',
-//     'table.cell.upMove': 'W',
-//     'table.cell.downMove': 'S',
-//     'table.cell.leftMove': 'A',
-//     'table.cell.rightMove': 'D',
-//     'table.row.current.topMove': 'Shift + W',
-//     'table.row.current.downMove': 'Shift + S',
-//     'pager.prevPage': 'ArrowLeft',
-//     'pager.nextPage': 'ArrowRight', // 单个按键
-//     'pager.prevJump': 'Ctrl + A',
-//     'pager.nextJump': 'Ctrl + D' // 组合键
-//   }
-// })
