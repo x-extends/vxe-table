1ce637b32cc39c18dd68e6ea035cf9ae37ae6dde
xuliangzhan <xu_liangzhan@163.com>
2020-09-19 19:30:47 +0800

    修复已知问题

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
       entry: 'examples/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      title: 'vxe-table 2.x'
+      title: 'vxe-table 2.x (LTS)'
     }
   },
   transpileDependencies: ['highlight.js'],
