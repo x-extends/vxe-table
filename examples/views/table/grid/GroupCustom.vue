9d77b98bf3ac550b7899513bcd3019ff007207d3
lijun <lijun_ay@126.com>
2020-10-23 12:58:09 +0800

    修改了包名称，为了自用。
    Signed-off-by: lijun <lijun_ay@126.com>

---
+++ examples/views/table/grid/GroupCustom.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <p class="tip">分组表头</p>
+    <vxe-grid
+      border
+	  resizable
+	  stripe
+      height="500"
+      :columns="tableColumn"
+      :useCustomHeaderRowSpan=true
+      :data="tableData"></vxe-grid>
+    <p class="demo-code">{{ $t('app.body.button.showCode') }}</p>
+
+    <pre>
+      <code class="xml">{{ demoCodes[0] }}</code>
+      <code class="javascript">{{ demoCodes[1] }}</code>
+    </pre>
+  </div>
+</template>
+<script>
+import hljs from 'highlight.js'
+
+export default {
+  data () {
+    return {
+      tableColumn: [
+        {
+          title: '序号',
+          customRowSpan: 3,
+          children: [{ type: 'seq', width: 50, title: '1' }]
+        },
+        {
+          title: '基本信息',
+          children: [
+            { title: 'Name', customRowSpan: 2, children: [{ field: 'name', title: '2' }] },
+            {
+              title: '其他信息',
+              children: [
+                { title: 'Nickname', children: [{ field: 'nickname', title: '3' }] },
+                { title: 'Age', children: [{ field: 'age', title: '4' }] }
+              ]
+            },
+            { title: 'Sex', customRowSpan: 2, children: [{ field: 'sex', title: '5' }] }
+          ]
+        },
+        { title: 'Address', customRowSpan: 3, children: [{ field: 'address', title: '6', showOverflow: true }] }
+      ],
+      tableData: [
+        { id: 10001, name: 'Test1', nickname: 'T1', role: 'Develop', sex: 'Man', age: 28, address: 'Shenzhen' },
+        { id: 10002, name: 'Test2', nickname: 'T2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
+        { id: 10003, name: 'Test3', nickname: 'T3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+        { id: 10004, name: 'Test4', nickname: 'T4', role: 'Designer', sex: 'Women ', age: 23, address: 'Shenzhen' },
+        { id: 10005, name: 'Test5', nickname: 'T5', role: 'Develop', sex: 'Women ', age: 30, address: 'Shanghai' },
+        { id: 10006, name: 'Test6', nickname: 'T6', role: 'Designer', sex: 'Women ', age: 21, address: 'Shenzhen' },
+        { id: 10007, name: 'Test7', nickname: 'T7', role: 'Test', sex: 'Man ', age: 29, address: 'Shenzhen' },
+        { id: 10008, name: 'Test8', nickname: 'T8', role: 'Develop', sex: 'Man ', age: 35, address: 'Shenzhen' }
+      ],
+      demoCodes: [
+        `
+        <vxe-grid
+          border
+          stripe
+          resizable
+          height="500"
+          :columns="tableColumn"
+          :useCustomHeaderRowSpan=true  // 使用定制表头行合并方式
+          :data="tableData"></vxe-grid>
+        `,
+        `
+        export default {
+          data () {
+            return {
+              tableColumn: [
+                {
+                  title: '序号',
+                  customRowSpan: 3,
+                  children: [{ type: 'seq', width: 50, title: '1' }]
+                },
+                {
+                  title: '基本信息',
+                  children: [
+                    { title: 'Name', customRowSpan: 2, children: [{ field: 'name', title: '2' }] },
+                    {
+                      title: '其他信息',
+                      children: [
+                        { title: 'Nickname', children: [{ field: 'nickname', title: '3' }] },
+                        { title: 'Age', children: [{ field: 'age', title: '4' }] }
+                      ]
+                    },
+                    { title: 'Sex', customRowSpan: 2, children: [{ field: 'sex', title: '5' }] }
+                  ]
+                },
+                { title: 'Address', customRowSpan: 3, children: [{ field: 'address', title: '6', showOverflow: true }] }
+              ],
+              tableData: [
+                { id: 10001, name: 'Test1', nickname: 'T1', role: 'Develop', sex: 'Man', age: 28, address: 'Shenzhen' },
+                { id: 10002, name: 'Test2', nickname: 'T2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
+                { id: 10003, name: 'Test3', nickname: 'T3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+                { id: 10004, name: 'Test4', nickname: 'T4', role: 'Designer', sex: 'Women ', age: 23, address: 'Shenzhen' },
+                { id: 10005, name: 'Test5', nickname: 'T5', role: 'Develop', sex: 'Women ', age: 30, address: 'Shanghai' },
+                { id: 10006, name: 'Test6', nickname: 'T6', role: 'Designer', sex: 'Women ', age: 21, address: 'Shenzhen' },
+                { id: 10007, name: 'Test7', nickname: 'T7', role: 'Test', sex: 'Man ', age: 29, address: 'Shenzhen' },
+                { id: 10008, name: 'Test8', nickname: 'T8', role: 'Develop', sex: 'Man ', age: 35, address: 'Shenzhen' }
+              ]
+            }
+          }
+        }
+        `
+      ]
+    }
+  },
+  mounted () {
+    Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
+      hljs.highlightBlock(block)
+    })
+  }
+}
+</script>
