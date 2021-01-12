c53b2a6ee1844de7c33ba0a77935e5cc2d35186a
John <John60676@qq.com>
2021-01-12 16:36:25 +0800

    refactor: update types

--- types/column.d.ts
+++ types/column.d.ts
@@ -6,6 +6,7 @@ import { ColumnHeaderSlotParams, ColumnHeaderRenderParams } from './extends/head
 import { ColumnFooterSlotParams, ColumnFooterRenderParams } from './extends/footer'
 import { ColumnEditRenderOptions, ColumnEditSlotParams } from './extends/edit'
 import { ColumnExportCellRenderParams, ColumnExportFooterRenderParams } from './extends/export'
+import { TableOverflow } from './table'
 
 /**
  * 列
@@ -196,15 +197,15 @@ export interface ColumnOptions {
   /**
    * 当内容过长时显示为省略号
    */
-  showOverflow?: boolean | string;
+  showOverflow?: TableOverflow;
   /**
    * 当表头内容过长时显示为省略号
    */
-  showHeaderOverflow?: boolean | string;
+  showHeaderOverflow?: TableOverflow;
   /**
    * 当表尾内容过长时显示为省略号
    */
-  showFooterOverflow?: boolean | string;
+  showFooterOverflow?: TableOverflow;
   /**
    * 给单元格附加 className
    */
