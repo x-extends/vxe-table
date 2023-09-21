e980a6e93443e52d217e0529a6025d3d3a0289ea
xuliangzhan <xu_liangzhan@163.com>
2023-09-21 08:34:04 +0800

    fix

--- packages/v-x-e-table/src/config.ts
+++ packages/v-x-e-table/src/config.ts
@@ -1,5 +1,6 @@
 import GlobalConfig from './conf'
 import XEUtils from 'xe-utils'
+import DomZIndex from 'dom-zindex'
 
 import { VxeGlobalConfigMethod } from '../../../types/all'
 
@@ -7,5 +8,8 @@ import { VxeGlobalConfigMethod } from '../../../types/all'
  * 全局参数设置
  */
 export const config: VxeGlobalConfigMethod = (options) => {
+  if (options && options.zIndex) {
+    DomZIndex.setCurrent(options.zIndex)
+  }
   return XEUtils.merge(GlobalConfig, options)
 }
