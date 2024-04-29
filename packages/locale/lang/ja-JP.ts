87d3e973bd22195948cfa7901f4935f63ff299dc
Chin Syu <405796690@qq.com>
2024-04-29 13:56:09 +0900

    日语版语言文件更新

--- packages/locale/lang/ja-JP.ts
+++ packages/locale/lang/ja-JP.ts
@@ -1,20 +1,14 @@
 export default {
   vxe: {
-    base: {
-      pleaseInput: 'Please input',
-      pleaseSelect: 'Select'
-    },
-    loading: {
-      text: 'Loading...'
-    },
     error: {
-      groupFixed: 'If you use group headers, the freeze columns must be set by group.',
+      groupFixed: 'グループのヘッダを使用すると、固定列はグループで設定しなければなりません',
       groupMouseRange: 'グループ化ヘッダーと「{0}」は同時に使用できません。これによりエラーが発生する可能性があります',
-      groupTag: '分组列头应该使用 "{0}" 而不是 "{1}"，这可能会出现错误',
-      scrollErrProp: '启用虚拟滚动后不支持该参数 "{0}"',
-      errConflicts: '参数 "{0}" 与 "{1}" 有冲突',
+      groupTag: 'グループ化ヘッダーは "{0}" を使ってください。 "{1}" はエラーが発生する可能性があります',
+      scrollErrProp: '仮想スクロールはこのパラメータを使えません： "{0}"',
+      scrollXNotGroup: '横軸の仮想スクロールはグループ化ヘッダーを使えません。 "scroll-x.enabled=false" パラメータを指定してください',
+      errConflicts: 'パラメータ "{0}" と "{1}" は同時に使えません',
       unableInsert: '指定された位置に挿入できない',
-      useErr: '"{0}" モジュールをインストールする際にエラーが発生し,順序が正しくない可能性があり,依頼するモジュールはTableの前にインストールする必要がある',
+      useErr: '"{0}" モジュールをインストールする際にエラーが発生し,順序が正しくない可能性があります。依存するモジュールはTableの前にインストールする必要があります',
       barUnableLink: 'ツールバーはフォームを関連付けることができない',
       expandContent: '拡張行スロットは "content" である必要があります。正しいかどうかを確認してください',
       reqModule: '"{0}" モジュールが必要',
@@ -23,58 +17,134 @@ export default {
       errProp: 'サポートされていないパラメーター "{0}"，"{1}" の可能性があります',
       colRepet: 'column.{0}="{0}" が重複しているため、機能が使えなくなることがあります。',
       notFunc: 'method "{0}" はありません',
-      errFunc: 'The argument "{0}" is not a method',
-      notValidators: 'Global validators "{0}" no existe.',
-      notFormats: 'Global formats "{0}" no existe.',
-      notCommands: 'Global commands "{0}" no existe.',
       notSlot: 'slot "{0}" はありません',
       noTree: 'Tree structureは "{0}" をサポートしていません',
       notProp: 'サポートされていないパラメータ "{0}"',
-      checkProp: '当数据量过大时可能会导致复选框卡顿，建议设置参数 "{0}" 提升渲染速度',
-      coverProp: 'The parameter "{1}" to "{0}" is repeatedly defined. This may cause an error',
+      coverProp: '"{0}" のパラメータ "{1}" が上書きされるため、エラーが発生する可能性があります',
       delFunc: '"{0}" functionは非推奨です、"{1}"を使用してください',
       delProp: '"{0}" propertyは非推奨です、"{1}"を使用してください',
       delEvent: 'イベント "{0}" は廃止されました。 "{1}" を使用してください',
       removeProp: 'パラメーター "{0}" は非推奨および非推奨です。エラーが発生する可能性があります',
-      errFormat: '全局的格式化内容应该使用 "VXETable.formats" 定义，挂载 "formatter={0}" 的方式已不建议使用',
-      notType: 'サポートされていないファイルの種類 "{0}"',
-      notExp: 'ブラウザはインポート/エクスポートをサポートしていません',
-      impFields: 'インポートに失敗しました。フィールド名とデータ形式が正しいかどうかを確認してください',
+      errFormat: '全般フォーマットするには "VXETable.formats" で定義してください。 "formatter={0}" での方式が非推奨です',
+      notType: 'サポートされていないファイル種類 "{0}"',
+      notExp: 'ご利用のブラウザはインポート/エクスポートをサポートしていません',
+      impFields: 'インポートに失敗しました。フィールド名とデータ形式を確認してください',
       treeNotImp: 'ツリーテーブルはインポートをサポートしていません'
+    },
+    renderer: {
+      search: '搜索',
+      cases: {
+        equal: 'イコール',
+        unequal: 'ノットイコール',
+        gt: '大なり',
+        ge: '大なりイコール',
+        lt: '小なり',
+        le: '小なりイコール',
+        begin: '先頭は',
+        notbegin: 'not begin',
+        endin: '末尾は',
+        notendin: 'not endin',
+        include: '含む',
+        exclude: '含まない',
+        between: '属する',
+        custom: 'カスタムフィルター',
+        insensitive: '大文字小文字区別しない',
+        isSensitive: '大文字小文字区別する'
+      },
+      combination: {
+        menus: {
+          sortAsc: '昇順',
+          sortDesc: '降順',
+          fixedColumn: '列ロック',
+          fixedGroup: 'グループロック',
+          cancelFixed: 'ロック解除',
+          fixedLeft: '左側ロック',
+          fixedRight: '右側ロック',
+          clearFilter: 'フィルタークリア',
+          textOption: '文字絞り込む',
+          numberOption: '数値絞り込み'
+        },
+        popup: {
+          title: 'カスタムフィルター',
+          currColumnTitle: '現在列：',
+          and: 'と',
+          or: 'もしくは',
+          describeHtml: ' ? で一文字<br/> * で複数文字を代替する'
+        },
+        empty: '(空白)',
+        notData: 'データなし'
+      }
+    },
+    pro: {
+      area: {
+        mergeErr: '結合セルに使えません',
+        multiErr: '複数選択されたセルに使えません',
+        extendErr: '結合セルが含まれている場合、すべての結合セルのサイズが同じでなければなりません'
+      },
+      fnr: {
+        title: '検索と置換',
+        findLabel: '検索',
+        replaceLabel: '置換',
+        findTitle: '検索内容：',
+        replaceTitle: '置換する文字：',
+        tabs: {
+          find: '検索',
+          replace: '置換'
+        },
+        filter: {
+          re: '正規表現',
+          whole: 'フルワード',
+          sensitive: '大文字小文字区別する'
+        },
+        btns: {
+          findNext: '次検索',
+          findAll: 'すべて検索',
+          replace: '置換',
+          replaceAll: 'すべて置換',
+          cancel: 'キャンセル'
+        },
+        header: {
+          seq: '#',
+          cell: 'セル',
+          value: '値'
+        },
+        empty: '(空白)',
+        reError: '無効な正規表現',
+        recordCount: '{0} 個のセルが見つかりました',
+        notCell: '見つかりませんでした',
+        replaceSuccess: '{0} 個のセルを置換しました'
+      }
     },
     table: {
       emptyText: 'データがありません',
       allTitle: '全て選択/取消',
       seqTitle: '#',
-      confirmFilter: '完了',
+      confirmFilter: 'ＯＫ',
       resetFilter: 'リセット',
       allFilter: '全て',
       sortAsc: '昇順',
       sortDesc: '降順',
-      filter: '選択した列をフィルタする',
-      impSuccess: '{0} レコードが正常にインポートされました',
-      expLoading: '正在导出中',
+      filter: '選択した列が絞り込む有効',
+      impSuccess: '{0} レコードがインポートされました',
+      expLoading: 'エクスポート中',
       expSuccess: 'エクスポートが成功しました',
       expOriginFilename: 'エクスポート_{0}',
       expSrcFilename: 'エクスポート_ソース_{0}',
-      customTitle: '列设置',
+      customTitle: '列設定',
       customAll: '全部',
-      customConfirm: '确认',
-      customRestore: 'リセット',
-      maxFixedCol: 'The maximum number of Freeze columns cannot exceed {0}'
+      customConfirm: 'ＯＫ',
+      customRestore: 'リセット'
     },
     grid: {
-      selectOneRecord: '少なくとも1つのレコードを選択してください',
+      selectOneRecord: '1つ以上ののレコードを選択してください',
       deleteSelectRecord: 'レコードを削除してもよろしいですか？',
       removeSelectRecord: 'レコードを削除してもよろしいですか？',
-      dataUnchanged: 'データは変更されませんでした',
+      dataUnchanged: 'データが変更されませんでした',
       delSuccess: '選択したレコードを削除しました',
       saveSuccess: '保存しました',
       operError: 'エラーが発生しました。操作が失敗しました'
     },
     select: {
-      search: 'Search',
-      loadingText: 'Loading',
       emptyText: 'データがありません'
     },
     pager: {
@@ -82,58 +152,30 @@ export default {
       pagesize: '{0}件/ページ',
       total: '全 {0} 件',
       pageClassifier: '',
-      homePage: 'Home',
-      homePageTitle: 'Home page',
       prevPage: '前のページ',
-      prevPageTitle: '前のページ',
       nextPage: '次のページ',
-      nextPageTitle: '次のページ',
       prevJump: '前のページに移動',
-      prevJumpTitle: '前のページに移動',
-      nextJump: '次のページに移動',
-      nextJumpTitle: '次のページに移動',
-      endPage: 'End',
-      endPageTitle: 'End page'
+      nextJump: '次のページに移動'
     },
     alert: {
-      title: 'System messages'
+      title: 'メッセージ'
     },
     button: {
       confirm: '完了',
       cancel: 'キャンセル'
-    },
-    filter: {
-      search: '搜索'
-    },
-    custom: {
-      cstmTitle: 'Column Settings',
-      cstmRestore: 'Restore default',
-      cstmCancel: 'Cancelar',
-      cstmConfirm: 'Confirm',
-      cstmConfirmRestore: 'Please confirm whether to restore the default column configuration?',
-      cstmDragTarget: 'Moving target: {0}',
-      setting: {
-        colSort: 'Sort',
-        colTitle: 'Title',
-        colVisible: 'Visible',
-        colFixed: 'Freeze column',
-        fixedLeft: 'Left',
-        fixedUnset: 'Unset',
-        fixedRight: 'Right'
-      }
     },
     import: {
       modes: {
         covering: 'カバー',
         insert: '追加'
       },
-      impTitle: '导入数据',
+      impTitle: 'データインポート',
       impFile: 'ファイル名',
       impSelect: 'ファイルを選択',
       impType: 'ファイルの種類',
-      impOpts: '参数设置',
+      impOpts: 'パラメータ設定',
       impConfirm: 'インポート',
-      impCancel: '取消'
+      impCancel: 'キャンセル'
     },
     export: {
       types: {
@@ -146,38 +188,38 @@ export default {
         pdf: 'PDF (*.pdf)'
       },
       modes: {
-        current: '現在のデータ(現在のページのデータ)',
+        current: '表示中のデータ(表示中のページのデータ)',
         selected: '選択したデータ(現在のページで選択したデータ)',
-        all: '全量データ（全ページ分のデータを含む）'
+        all: '全てのデータ（全てのページのデータを含む）'
       },
-      printTitle: '打印数据',
-      expTitle: '导出数据',
+      printTitle: '印刷',
+      expTitle: 'データ',
       expName: 'ファイル名',
       expNamePlaceholder: 'ファイル名を入力してください',
       expSheetName: 'タイトル',
       expSheetNamePlaceholder: 'タイトルを入力してください',
       expType: '保存タイプ',
-      expMode: '选择字段',
+      expMode: '保存データ',
       expCurrentColumn: '全てのフィールド',
-      expColumn: '选择字段',
-      expOpts: '参数设置',
+      expColumn: '列',
+      expOpts: 'パラメータ',
       expOptHeader: 'ヘッダー',
       expHeaderTitle: 'ヘッダーをエクスポート',
       expOptFooter: 'フッター',
       expFooterTitle: 'フッターをエクスポート',
-      expOptColgroup: '分组表头',
-      expColgroupTitle: '如果存在，则支持带有分组结构的表头',
-      expOptMerge: '合并',
-      expMergeTitle: '如果存在，则支持带有合并结构的单元格',
-      expOptAllExpand: '展开层级',
-      expAllExpandTitle: '如果存在，则支持将带有层级结构的数据全部展开',
-      expOptUseStyle: '样式',
-      expUseStyleTitle: '如果存在，则支持带样式的单元格',
+      expOptColgroup: 'グループ分けタイトル',
+      expColgroupTitle: 'グループ分けタイトルを有効にする',
+      expOptMerge: '結合',
+      expMergeTitle: '結合セルを有効にする',
+      expOptAllExpand: '階層構造',
+      expAllExpandTitle: '階層構造を有効にする',
+      expOptUseStyle: 'スタイル',
+      expUseStyleTitle: 'スタイルを有効にする',
       expOptOriginal: 'ソースデータ',
-      expOriginalTitle: '如果为源数据，则支持导入到表格中',
+      expOriginalTitle: 'ソースデータを有効にする',
       expPrint: '印刷',
-      expConfirm: 'エクスポート',
-      expCancel: '取消'
+      expConfirm: 'ＯＫ',
+      expCancel: 'キャンセル'
     },
     modal: {
       zoomIn: '最大化',
@@ -189,7 +231,7 @@ export default {
       unfolding: '展開'
     },
     toolbar: {
-      import: 'インポート',
+      import: 'ファイル読込',
       export: 'エクスポート',
       print: '印刷',
       refresh: '再読み込み',
@@ -197,11 +239,8 @@ export default {
       zoomOut: '縮小',
       custom: 'カスタム設定',
       customAll: '全部',
-      customConfirm: '確定',
-      customRestore: 'リセット',
-      fixedLeft: 'Freeze on the left',
-      fixedRight: 'Freeze on the right',
-      cancelfixed: 'Unfreeze column'
+      customConfirm: 'OK',
+      customRestore: '戻る'
     },
     input: {
       date: {
@@ -226,7 +265,7 @@ export default {
           datetime: 'yyyy-MM-dd HH:mm:ss',
           week: 'yyyy 年 WW',
           month: 'yyyy-MM',
-          quarter: 'yyyy 年第 q 季度',
+          quarter: 'yyyy 年第 q 四半期',
           year: 'yyyy'
         },
         weeks: {
@@ -254,210 +293,11 @@ export default {
           m11: '12月'
         },
         quarters: {
-          q1: '第一季度',
-          q2: '第二季度',
-          q3: '第三季度',
-          q4: '第四季度'
+          q1: '第1四半期',
+          q2: '第2四半期',
+          q3: '第3四半期',
+          q4: '第4四半期'
         }
-      }
-    },
-
-    /**
-     * 扩展插件
-     */
-    plugins: {
-      extendCellArea: {
-        area: {
-          mergeErr: '无法对合并单元格进行该操作',
-          multiErr: '无法对多重选择区域进行该操作',
-          extendErr: '如果延伸的区域包含被合并的单元格，所有合并的单元格需大小相同',
-          pasteMultiErr: '无法粘贴，需要相同大小的复制的区域和粘贴的区域才能执行此操作',
-          cpInvalidErr: '该操作无法进行，您选择的区域中存在被禁止的列（{0}）'
-        },
-        fnr: {
-          title: '查找和替换',
-          findLabel: '查找',
-          replaceLabel: '替换',
-          findTitle: '查找内容：',
-          replaceTitle: '替换为：',
-          tabs: {
-            find: '查找',
-            replace: '替换'
-          },
-          filter: {
-            re: '正则表达式',
-            whole: '全词匹配',
-            sensitive: '区分大小写'
-          },
-          btns: {
-            findNext: '查找下一个',
-            findAll: '查找全部',
-            replace: '替换',
-            replaceAll: '替换全部',
-            cancel: '取消'
-          },
-          header: {
-            seq: '#',
-            cell: '单元格',
-            value: '值'
-          },
-          empty: '(空值)',
-          reError: '无效的正则表达式',
-          recordCount: '已找到 {0} 个单元格',
-          notCell: '找不到匹配的单元格',
-          replaceSuccess: '成功替换 {0} 个单元格'
-        }
-      },
-      filterComplexInput: {
-        menus: {
-          fixedColumn: '锁定列',
-          fixedGroup: '锁定组',
-          cancelFixed: '取消锁定',
-          fixedLeft: '锁定左侧',
-          fixedRight: '锁定右侧'
-        },
-        cases: {
-          equal: '等于',
-          gt: '大于',
-          lt: '小于',
-          begin: '开头是',
-          endin: '结尾是',
-          include: '包含',
-          isSensitive: '区分大小写'
-        }
-      },
-      filterCombination: {
-        menus: {
-          clearSort: '清除排序',
-          sortAsc: '升序',
-          sortDesc: '降序',
-          fixedColumn: '锁定列',
-          fixedGroup: '锁定组',
-          cancelFixed: '取消锁定',
-          fixedLeft: '锁定左侧',
-          fixedRight: '锁定右侧',
-          clearFilter: '清除筛选',
-          textOption: '文本筛选',
-          numberOption: '数值筛选'
-        },
-        popup: {
-          title: '自定义筛选的方式',
-          currColumnTitle: '当前列：',
-          and: '与',
-          or: '或',
-          describeHtml: '可用 ? 代表单个字符<br/>用 * 代表任意多个字符'
-        },
-        cases: {
-          equal: '等于',
-          unequal: '不等于',
-          gt: '大于',
-          ge: '大于或等于',
-          lt: '小于',
-          le: '小于或等于',
-          begin: '开头是',
-          notbegin: '开头不是',
-          endin: '结尾是',
-          notendin: '结尾不是',
-          include: '包含',
-          exclude: '不包含',
-          between: '介于',
-          custom: '自定义筛选',
-          insensitive: '不区分大小写',
-          isSensitive: '区分大小写'
-        },
-        empty: '(空白)',
-        notData: '无匹配项'
-      }
-    },
-
-    /**
-     * 以下废弃
-     * @deprecated
-     */
-    renderer: {
-      search: '搜索',
-      cases: {
-        equal: '等于',
-        unequal: '不等于',
-        gt: '大于',
-        ge: '大于或等于',
-        lt: '小于',
-        le: '小于或等于',
-        begin: '开头是',
-        notbegin: '开头不是',
-        endin: '结尾是',
-        notendin: '结尾不是',
-        include: '包含',
-        exclude: '不包含',
-        between: '介于',
-        custom: '自定义筛选',
-        insensitive: '不区分大小写',
-        isSensitive: '区分大小写'
-      },
-      combination: {
-        menus: {
-          clearSort: '清除排序',
-          sortAsc: '升序',
-          sortDesc: '降序',
-          fixedColumn: '锁定列',
-          fixedGroup: '锁定组',
-          cancelFixed: '取消锁定',
-          fixedLeft: '锁定左侧',
-          fixedRight: '锁定右侧',
-          clearFilter: '清除筛选',
-          textOption: '文本筛选',
-          numberOption: '数值筛选'
-        },
-        popup: {
-          title: '自定义筛选的方式',
-          currColumnTitle: '当前列：',
-          and: '与',
-          or: '或',
-          describeHtml: '可用 ? 代表单个字符<br/>用 * 代表任意多个字符'
-        },
-        empty: '(空白)',
-        notData: '无匹配项'
-      }
-    },
-    pro: {
-      area: {
-        mergeErr: '无法对合并单元格进行该操作',
-        multiErr: '无法对多重选择区域进行该操作',
-        extendErr: '如果延伸的区域包含被合并的单元格，所有合并的单元格需大小相同',
-        pasteMultiErr: '无法粘贴，需要相同大小的复制的区域和粘贴的区域才能执行此操作'
-      },
-      fnr: {
-        title: '查找和替换',
-        findLabel: '查找',
-        replaceLabel: '替换',
-        findTitle: '查找内容：',
-        replaceTitle: '替换为：',
-        tabs: {
-          find: '查找',
-          replace: '替换'
-        },
-        filter: {
-          re: '正则表达式',
-          whole: '全词匹配',
-          sensitive: '区分大小写'
-        },
-        btns: {
-          findNext: '查找下一个',
-          findAll: '查找全部',
-          replace: '替换',
-          replaceAll: '替换全部',
-          cancel: '取消'
-        },
-        header: {
-          seq: '#',
-          cell: '单元格',
-          value: '值'
-        },
-        empty: '(空值)',
-        reError: '无效的正则表达式',
-        recordCount: '已找到 {0} 个单元格',
-        notCell: '找不到匹配的单元格',
-        replaceSuccess: '成功替换 {0} 个单元格'
       }
     }
   }
