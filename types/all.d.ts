efd89574f42599e2561da084fc9656d425c966f6
xuliangzhan <xlz26296x@163.com>
2024-06-18 22:51:55 +0800

    releases 4.7.25

--- types/all.d.ts
+++ types/all.d.ts
@@ -24,6 +24,7 @@ export type VXETableCore = VxeUIExport
 
 export function install (app: App, options?: VxeGlobalConfig): void
 
+// Vxe core
 export * from 'vxe-pc-ui/types/ui'
 
 // Vxe Table
@@ -32,3 +33,19 @@ export * from 'vxe-pc-ui/types/components/colgroup'
 export * from 'vxe-pc-ui/types/components/table'
 export * from 'vxe-pc-ui/types/components/grid'
 export * from 'vxe-pc-ui/types/components/toolbar'
+
+// 已废弃，兼容老版本类型
+export * from 'vxe-pc-ui/types/components/textarea'
+export * from 'vxe-pc-ui/types/components/select'
+export * from 'vxe-pc-ui/types/components/switch'
+export * from 'vxe-pc-ui/types/components/optgroup'
+export * from 'vxe-pc-ui/types/components/option'
+export * from 'vxe-pc-ui/types/components/pager'
+export * from 'vxe-pc-ui/types/components/form'
+export * from 'vxe-pc-ui/types/components/form-item'
+export * from 'vxe-pc-ui/types/components/modal'
+export * from 'vxe-pc-ui/types/components/button'
+export * from 'vxe-pc-ui/types/components/button-group'
+export * from 'vxe-pc-ui/types/components/input'
+export * from 'vxe-pc-ui/types/components/pulldown'
+export * from 'vxe-pc-ui/types/components/tooltip'
