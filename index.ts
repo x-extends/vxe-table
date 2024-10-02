a1006b6a3d8de30aa805b7ad1d5f06d728106755
xuliangzhan <xlz26296x@163.com>
2024-10-02 19:29:06 +0800

    fix

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import * as VxeTableExport from './packages/components'
 import './styles/all.scss'
 
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(VxeTableExport)
+}
+
 export * from './packages/components'
 export default VxeTableExport
