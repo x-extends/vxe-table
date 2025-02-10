a0b777f2adb5b7a379bd0e73bd90d8de62af7acd
xuliangzhan <xu_liangzhan@163.com>
2025-02-10 13:17:55 +0800

    releases 4.10.11

--- packages/table/src/header.ts
+++ packages/table/src/header.ts
@@ -115,8 +115,10 @@ export default defineComponent({
         const isAutoCellWidth = !column.resizeWidth && (column.minWidth === 'auto' || column.width === 'auto')
 
         let isVNPreEmptyStatus = false
-        if (scrollXLoad && !column.fixed && (_columnIndex < scrollXStore.visibleStartIndex - scrollXStore.preloadSize || _columnIndex > scrollXStore.visibleEndIndex + scrollXStore.preloadSize)) {
-          isVNPreEmptyStatus = true
+        if (!isGroup) {
+          if (scrollXLoad && !column.fixed && (_columnIndex < scrollXStore.visibleStartIndex - scrollXStore.preloadSize || _columnIndex > scrollXStore.visibleEndIndex + scrollXStore.preloadSize)) {
+            isVNPreEmptyStatus = true
+          }
         }
 
         const tcStyle: Record<string, string> = {}
