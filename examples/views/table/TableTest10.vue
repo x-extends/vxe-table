ad071fd889750de0be0a57fc26e6cbc430f4756d
xuliangzhan <xu_liangzhan@163.com>
2026-02-01 15:30:07 +0800

    releases 4.18.0

--- examples/views/table/TableTest10.vue
+++ examples/views/table/TableTest10.vue
@@ -1,74 +1,73 @@
 <template>
   <div>
-    <vxe-grid v-bind="gridOptions"></vxe-grid>
+    <vxe-table
+      border
+      ref="tableRef"
+      :row-config="rowConfig"
+      :row-drag-config="rowDragConfig"
+      :column-config="columnConfig"
+      :tree-config="treeConfig"
+      :data="tableData">
+      <vxe-column type="seq" width="70"></vxe-column>
+      <vxe-column field="name" title="Name" min-width="300" tree-node drag-sort></vxe-column>
+      <vxe-column field="size" title="Size"></vxe-column>
+      <vxe-column field="type" title="Type"></vxe-column>
+      <vxe-column field="date" title="Date"></vxe-column>
+    </vxe-table>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { reactive } from 'vue'
-import type { VxeGridProps, VxeTablePropTypes } from '../../../types'
-
-interface RowVO {
-  id: number
-  name: string
-  role: string
-  sex: string
-  age: number
-  address: string
+<script>
+export default {
+  data () {
+    const tableData = [
+      { id: 10000, parentId: null, name: 'Test1', type: 'mp3', size: 1024, date: '2020-08-01' },
+      { id: 10050, parentId: null, name: 'Test2', type: 'mp4', size: 0, date: '2021-04-01' },
+      { id: 24300, parentId: 10050, name: 'Test3', type: 'avi', size: 1024, date: '2020-03-01' },
+      { id: 20045, parentId: 24300, name: 'Test4', type: 'html', size: 600, date: '2021-04-01' },
+      { id: 10053, parentId: 24300, name: 'Test5', type: 'avi', size: 0, date: '2021-04-01' },
+      { id: 24330, parentId: 10053, name: 'Test6', type: 'txt', size: 25, date: '2021-10-01' },
+      { id: 21011, parentId: 10053, name: 'Test7', type: 'pdf', size: 512, date: '2020-01-01' },
+      { id: 22200, parentId: 10053, name: 'Test8', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 23666, parentId: null, name: 'Test9', type: 'xlsx', size: 2048, date: '2020-11-01' },
+      { id: 23677, parentId: 23666, name: 'Test10', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 23671, parentId: 23677, name: 'Test11', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 23672, parentId: 23677, name: 'Test12', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 23688, parentId: 23666, name: 'Test13', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 23681, parentId: 23688, name: 'Test14', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 23682, parentId: 23688, name: 'Test15', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 24555, parentId: null, name: 'Test16', type: 'avi', size: 224, date: '2020-10-01' },
+      { id: 24566, parentId: 24555, name: 'Test17', type: 'js', size: 1024, date: '2021-06-01' },
+      { id: 24577, parentId: 24555, name: 'Test18', type: 'js', size: 1024, date: '2021-06-01' }
+    ]
+    const rowConfig = {
+      drag: true
+    }
+    const rowDragConfig = {
+      isCrossDrag: true
+    }
+    const columnConfig = {}
+    const treeConfig = {
+      transform: true,
+      rowField: 'id',
+      parentField: 'parentId'
+    }
+    return {
+      tableData,
+      rowConfig,
+      rowDragConfig,
+      columnConfig,
+      treeConfig
+    }
+  },
+  methods: {
+    resultEvent () {
+      const $table = this.$refs.tableRef
+      if ($table) {
+        const tableData = $table.getFullData()
+        console.log(tableData)
+      }
+    }
+  }
 }
-
-const gridOptions = reactive<VxeGridProps<RowVO> & { keyboardConfig: VxeTablePropTypes.KeyboardConfig<RowVO> }>({
-  border: true,
-  showOverflow: true,
-  keepSource: true,
-  height: 400,
-  mouseConfig: {
-    selected: true
-  },
-  keyboardConfig: {
-    isEdit: true,
-    isArrow: true,
-    isEnter: true,
-    isTab: true,
-    isDel: true,
-    isBack: true,
-    isEsc: true,
-    enterToTab: true,
-    isLastEnterAppendRow: true,
-    isLastTabAppendRow: true
-  },
-  editConfig: {
-    trigger: 'dblclick',
-    mode: 'cell',
-    showStatus: true
-  },
-  columns: [
-    { type: 'seq', width: 70 },
-    { field: 'name', title: 'Name', editRender: { name: 'VxeInput' } },
-    { field: 'sex', title: 'Sex', editRender: { name: 'VxeInput' } },
-    { field: 'age', title: 'Age', editRender: { name: 'VxeInput' } }
-  ],
-  data: [
-    { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
-    { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
-    { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
-    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 23, address: 'test abc' },
-    { id: 10005, name: 'Test5', role: 'Develop', sex: 'Women', age: 30, address: 'Shanghai' },
-    { id: 10006, name: 'Test6', role: 'Designer', sex: 'Women', age: 21, address: 'test abc' },
-    { id: 10007, name: 'Test7', role: 'Test', sex: 'Man', age: 29, address: 'test abc' },
-    { id: 10008, name: 'Test8', role: 'Develop', sex: 'Man', age: 35, address: 'test abc' },
-    { id: 10009, name: 'Test9', role: 'Test', sex: 'Man', age: 26, address: 'test abc' },
-    { id: 10010, name: 'Test10', role: 'Develop', sex: 'Man', age: 38, address: 'test abc' },
-    { id: 10011, name: 'Test11', role: 'Test', sex: 'Women', age: 29, address: 'test abc' },
-    { id: 10012, name: 'Test12', role: 'Develop', sex: 'Man', age: 27, address: 'test abc' },
-    { id: 10013, name: 'Test13', role: 'Test', sex: 'Women', age: 24, address: 'test abc' },
-    { id: 10014, name: 'Test14', role: 'Develop', sex: 'Man', age: 34, address: 'test abc' },
-    { id: 10015, name: 'Test15', role: 'Test', sex: 'Man', age: 21, address: 'test abc' },
-    { id: 10016, name: 'Test16', role: 'Develop', sex: 'Women', age: 20, address: 'test abc' },
-    { id: 10017, name: 'Test17', role: 'Test', sex: 'Man', age: 31, address: 'test abc' },
-    { id: 10018, name: 'Test18', role: 'Develop', sex: 'Women', age: 32, address: 'test abc' },
-    { id: 10019, name: 'Test19', role: 'Test', sex: 'Man', age: 37, address: 'test abc' },
-    { id: 10020, name: 'Test20', role: 'Develop', sex: 'Man', age: 41, address: 'test abc' }
-  ]
-})
 </script>
